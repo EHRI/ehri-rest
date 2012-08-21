8bb0c25e57f12d74db0b1b2c91e9e2d945b93633
Mike Bryant <mikesname@gmail.com>
2012-08-22 00:11:08 +0100

    Add a chainable IndexNotFoundException constructor.

--- src/main/java/eu/ehri/project/exceptions/IndexNotFoundException.java
+++ src/main/java/eu/ehri/project/exceptions/IndexNotFoundException.java
@@ -10,4 +10,8 @@ public class IndexNotFoundException extends Exception {
     public IndexNotFoundException(String message) {
         super(message);
     }
+
+    public IndexNotFoundException(String message, Exception cause) {
+        super(message, cause);
+    }
 }
