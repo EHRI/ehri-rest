a648d4d9e994b298a08e9d07a5dc251112b1348f
Mike Bryant <mikesname@gmail.com>
2012-08-28 16:31:24 +0100

    Add a serialization exception.

---
+++ src/main/java/eu/ehri/project/exceptions/SerializationError.java
@@ -0,0 +1,14 @@
+package eu.ehri.project.exceptions;
+
+public class SerializationError extends Exception {
+
+    /**
+     * Serialization was attempted with input that did not match the
+     * required specifications.
+     */
+    private static final long serialVersionUID = -1664595600301157596L;
+
+    public SerializationError(String message) {
+        super(message);
+    }
+}
