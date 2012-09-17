6821c9f6ce4b7c48fb6eec6677afa74954b6e1c2
Mike Bryant <mikesname@gmail.com>
2012-09-17 09:45:43 +0100

    Remove properties that should be optional on userProfile

--- src/main/java/eu/ehri/project/models/UserProfile.java
+++ src/main/java/eu/ehri/project/models/UserProfile.java
@@ -26,22 +26,4 @@ public interface UserProfile extends VertexFrame, Accessor, AccessibleEntity, An
 
     @Property("name")
     public void setName(String name);
-
-    @Property("about")
-    public String getAbout();
-
-    @Property("about")
-    public void setAbout(String name);
-
-    @Property("languages")
-    public String[] getLanguages();
-
-    @Property("languages")
-    public void setLanguages(String[] languages);
-
-    @Property("location")
-    public String getLocation();
-
-    @Property("location")
-    public void setLocation(String location);
 }
