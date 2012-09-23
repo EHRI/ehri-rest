f9544293cfeceb4bfda166e09a0e66877ec1ce29
Mike Bryant <mikesname@gmail.com>
2012-09-23 12:49:35 +0100

    Remove duplicated FixtureLoader class in the plugin, instead using the one from the frames test utils.

--- ehri-plugin/src/test/java/eu/ehri/plugin/test/EhriNeo4jPluginTest.java
+++ ehri-plugin/src/test/java/eu/ehri/plugin/test/EhriNeo4jPluginTest.java
@@ -2,6 +2,9 @@ package eu.ehri.plugin.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -9,6 +12,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.server.rest.repr.NodeRepresentation;
 import org.neo4j.server.rest.repr.Representation;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
@@ -18,10 +22,10 @@ import com.tinkerpop.frames.FramedGraph;
 import eu.ehri.project.exceptions.IndexNotFoundException;
 import eu.ehri.project.exceptions.PermissionDenied;
 import eu.ehri.project.models.DocumentaryUnit;
+import eu.ehri.project.test.utils.FixtureLoader;
 import eu.ehri.project.views.Views;
 
 import eu.ehri.plugin.EhriNeo4jPlugin;
-import eu.ehri.plugin.test.utils.FixtureLoader;
 
 public class EhriNeo4jPluginTest {
 	protected FramedGraph<Neo4jGraph> graph;
@@ -29,11 +33,12 @@ public class EhriNeo4jPluginTest {
 	protected FixtureLoader helper;
 	protected EhriNeo4jPlugin plugin;
 	protected GraphDatabaseService db;
-	
+
 	// Members closely coupled to the test data!
 	protected Long validUserId = 20L;
 	protected Long invalidUserId = 21L;
 	protected Long itemId = 1L;
+	protected String itemName = "Test Collection 1";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -47,7 +52,7 @@ public class EhriNeo4jPluginTest {
 		helper = new FixtureLoader(graph);
 		helper.loadTestData();
 		views = new Views<DocumentaryUnit>(graph, DocumentaryUnit.class);
-		
+
 		plugin = new EhriNeo4jPlugin();
 		db = graph.getBaseGraph().getRawGraph();
 	}
@@ -57,26 +62,24 @@ public class EhriNeo4jPluginTest {
 		// graph.shutdown();
 	}
 
-    // lets simple things using the fixture
-    @Test(expected=IndexNotFoundException.class)
-    public void createIndexedVertex() throws Exception {
-    	Representation unit = plugin.getVertexIndex(db, "nonexistingindex");
-    }
+	// lets simple things using the fixture
+	@Test(expected = IndexNotFoundException.class)
+	public void createIndexedVertex() throws Exception {
+		Representation unit = plugin.getVertexIndex(db, "nonexistingindex");
+	}
 
-	// lets see If I can do the same details test as already done in the ehri-frames test 
-    @Test
-    public void testDetail() throws PermissionDenied {
-        DocumentaryUnit unit = views.detail(itemId, validUserId);
-        assertEquals(itemId, unit.asVertex().getId());
-    }
-    
-    // Then do the same but via the plugin
-	//@Ignore
-    @Test
-    public void testGetDocumentartyUnit() throws Exception {
-    	Representation unit = plugin.getDocumentaryUnit(db, itemId, validUserId);  
-    }
-    
+	// lets see If I can do the same details test as already done in the
+	// ehri-frames test
+	@Test
+	public void testDetail() throws PermissionDenied {
+		DocumentaryUnit unit = views.detail(itemId, validUserId);
+		assertEquals(itemId, unit.asVertex().getId());
+	}
 
-    
+	// Then do the same but via the plugin
+	@Test
+	public void testGetDocumentartyUnit() throws Exception {
+		Representation unit = plugin
+				.getDocumentaryUnit(db, itemId, validUserId);
+	}
 }
