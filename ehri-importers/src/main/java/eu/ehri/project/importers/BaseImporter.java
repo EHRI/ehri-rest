d731de72e673d3936dfb1c70979f37111c796c71
Mike Bryant <mikesname@gmail.com>
2012-09-24 21:04:59 +0100

    More sketchy initial work on programmatic importers.

--- ehri-importers/src/main/java/eu/ehri/project/importers/BaseImporter.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/BaseImporter.java
@@ -6,9 +6,8 @@ import java.util.List;
 
 import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 import com.tinkerpop.frames.FramedGraph;
-import com.tinkerpop.frames.VertexFrame;
-
 import eu.ehri.project.exceptions.ValidationError;
+import eu.ehri.project.models.Agent;
 import eu.ehri.project.models.Authority;
 import eu.ehri.project.models.DatePeriod;
 import eu.ehri.project.models.DocumentDescription;
@@ -17,63 +16,95 @@ import eu.ehri.project.persistance.BundleDAO;
 import eu.ehri.project.persistance.BundleFactory;
 import eu.ehri.project.persistance.EntityBundle;
 
-public class BaseImporter<T> implements Importer<T> {
-    private Object repositoryId;
-    private FramedGraph<Neo4jGraph> framedGraph;
+public abstract class BaseImporter<T> implements Importer<T> {
+	private Agent repository;
+	private FramedGraph<Neo4jGraph> framedGraph;
 
-    public BaseImporter(FramedGraph<Neo4jGraph> framedGraph, Object repositoryId) {
-        this.repositoryId = repositoryId;
-        this.framedGraph = framedGraph;
-    }
+	public BaseImporter(FramedGraph<Neo4jGraph> framedGraph, Agent repository) {
+		this.repository = repository;
+		this.framedGraph = framedGraph;
+	}
 
-    public void importDocumentaryUnit(T data) throws Exception {
-        // TODO Auto-generated method stub
+	public void importDocumentaryUnit(T data) throws Exception {
+		// TODO Auto-generated method stub
+	}
 
-    }
+	public List<T> extractChildData(T data) {
+		return new LinkedList<T>();
+	}
 
-    public EntityBundle<DocumentaryUnit> extractDocumentaryUnit(T data)
-            throws ValidationError {
-        return new BundleFactory<DocumentaryUnit>().buildBundle(
-                new HashMap<String, Object>(), DocumentaryUnit.class);
-    }
+	public EntityBundle<DocumentaryUnit> extractDocumentaryUnit(T data)
+			throws ValidationError {
 
-    public List<EntityBundle<DocumentDescription>> extractDocumentDescriptions(
-            T data) {
-        return new LinkedList<EntityBundle<DocumentDescription>>();
-    }
+		EntityBundle<DocumentaryUnit> bundle = new BundleFactory<DocumentaryUnit>()
+				.buildBundle(new HashMap<String, Object>(),
+						DocumentaryUnit.class);
 
-    public List<EntityBundle<DocumentaryUnit>> extractParent(T data) {
-        return new LinkedList<EntityBundle<DocumentaryUnit>>();
-    }
+		// Extract details for the logical item here
 
-    public List<EntityBundle<Authority>> extractAuthorities(T data) {
-        return new LinkedList<EntityBundle<Authority>>();
-    }
+		return bundle;
+	}
 
-    public List<EntityBundle<DatePeriod>> extractDates(T data) {
-        return new LinkedList<EntityBundle<DatePeriod>>();
-    }
+	public List<EntityBundle<DocumentDescription>> extractDocumentDescriptions(
+			T data) {
+		return new LinkedList<EntityBundle<DocumentDescription>>();
+	}
 
-    public void importDetails(T data) throws Exception {
-        EntityBundle<DocumentaryUnit> unit = extractDocumentaryUnit(data);
-        BundleDAO<DocumentaryUnit> persister = new BundleDAO<DocumentaryUnit>(framedGraph);
-        DocumentaryUnit frame = persister.insert(unit);
-        
-        // Save DatePeriods
-        {
-            BundleDAO<DatePeriod> datePersister = new BundleDAO<DatePeriod>(framedGraph);
-            for (EntityBundle<DatePeriod> dpb : extractDates(data)) {
-                frame.addDatePeriod(datePersister.insert(dpb));
-            }            
-        }
+	public List<EntityBundle<DocumentaryUnit>> extractParent(T data) {
+		return new LinkedList<EntityBundle<DocumentaryUnit>>();
+	}
 
-        // Save Descriptions
-        {
-            BundleDAO<DocumentDescription> descPersister = new BundleDAO<DocumentDescription>(framedGraph);
-            for (EntityBundle<DocumentDescription> dpb : extractDocumentDescriptions(data)) {
-                frame.addDescription(descPersister.insert(dpb));
-            }            
-        }
+	public List<EntityBundle<Authority>> extractAuthorities(T data) {
+		return new LinkedList<EntityBundle<Authority>>();
+	}
 
-    }    
+	public List<EntityBundle<DatePeriod>> extractDates(T data) {
+		return new LinkedList<EntityBundle<DatePeriod>>();
+	}
+
+	public void importDetails(T data, DocumentaryUnit parent) throws Exception {
+		EntityBundle<DocumentaryUnit> unit = extractDocumentaryUnit(data);
+		BundleDAO<DocumentaryUnit> persister = new BundleDAO<DocumentaryUnit>(
+				framedGraph);
+		DocumentaryUnit frame = persister.insert(unit);
+
+		// Set the parent child relationship
+		if (parent != null)
+			parent.addChild(frame);
+
+		// Set the repository/item relationship
+		repository.addCollection(frame);
+
+		// Save DatePeriods... this is not an idempotent step, and will need
+		// to be radically altered when updating existing items...
+		{
+			BundleDAO<DatePeriod> datePersister = new BundleDAO<DatePeriod>(
+					framedGraph);
+			for (EntityBundle<DatePeriod> dpb : extractDates(data)) {
+				frame.addDatePeriod(datePersister.insert(dpb));
+			}
+		}
+
+		// Save Descriptions
+		{
+			BundleDAO<DocumentDescription> descPersister = new BundleDAO<DocumentDescription>(
+					framedGraph);
+			for (EntityBundle<DocumentDescription> dpb : extractDocumentDescriptions(data)) {
+				frame.addDescription(descPersister.insert(dpb));
+			}
+		}
+
+		// Search through child parts and add them recursively...
+		for (T child : extractChildData(data)) {
+			importDetails(child, frame);
+		}
+
+	}
+
+	/**
+	 * Entry point for a top-level DocumentaryUnit item.
+	 */
+	public void importDetails(T data) throws Exception {
+		importDetails(data, null);
+	}
 }
--- ehri-importers/src/main/java/eu/ehri/project/importers/EadImporter.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/EadImporter.java
@@ -3,16 +3,15 @@ package eu.ehri.project.importers;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.apache.commons.collections.map.HashedMap;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -22,73 +21,93 @@ import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 import com.tinkerpop.frames.FramedGraph;
 
 import eu.ehri.project.exceptions.ValidationError;
+import eu.ehri.project.models.Agent;
 import eu.ehri.project.models.DocumentaryUnit;
 import eu.ehri.project.persistance.BundleFactory;
 import eu.ehri.project.persistance.EntityBundle;
 
 public class EadImporter extends BaseImporter<Node> {
 
-    private final XPath xpath;
+	private final XPath xpath;
 
-    public EadImporter(FramedGraph<Neo4jGraph> framedGraph, Object repositoryId) {
-        super(framedGraph, repositoryId);
-        xpath = XPathFactory.newInstance().newXPath();
+	public EadImporter(FramedGraph<Neo4jGraph> framedGraph, Agent repository) {
+		super(framedGraph, repository);
+		xpath = XPathFactory.newInstance().newXPath();
 
-    }
+	}
 
-    @Override
-    public void importDocumentaryUnit(Node data) throws Exception {
-        // TODO Auto-generated method stub
-        XPathExpression expr = xpath
-                .compile(".//archdesc/did/unittitle/text()");
-        System.out.println(expr.evaluate(data, XPathConstants.STRING));
-    }
+	// Pattern for EAD nodes that represent a child item
+	Pattern childItemPattern = Pattern.compile("^c(?:\\d+)$");
 
-    @Override
-    public EntityBundle<DocumentaryUnit> extractDocumentaryUnit(Node data)
-            throws ValidationError {
-        EntityBundle<DocumentaryUnit> unit = new BundleFactory<DocumentaryUnit>()
-                .buildBundle(new HashMap<String, Object>(),
-                        DocumentaryUnit.class);
-        
-        
-        return unit;
-    }
+	@Override
+	public void importDocumentaryUnit(Node data) throws Exception {
+		// TODO Auto-generated method stub
+		XPathExpression expr = xpath
+				.compile(".//archdesc/did/unittitle/text()");
+		System.out.println(expr.evaluate(data, XPathConstants.STRING));
+	}
 
-    public static void main(String[] args) throws Exception {
+	public List<Node> extractChildData(Node data) {
+		List<Node> children = new LinkedList<Node>();
+		for (int i = 0; i < data.getChildNodes().getLength(); i++) {
+			Node n = data.getChildNodes().item(i);
+			if (childItemPattern.matcher(n.getNodeName()).matches()) {
+				children.add(n);
+			}
+		}
+		return children;
+	}
 
-        if (args.length != 2)
-            throw new RuntimeException(
-                    "Usage: importer <ead.xml> <neo4j-graph-dir>");
+	@Override
+	public EntityBundle<DocumentaryUnit> extractDocumentaryUnit(Node data)
+			throws ValidationError {
+		EntityBundle<DocumentaryUnit> unit = new BundleFactory<DocumentaryUnit>()
+				.buildBundle(new HashMap<String, Object>(),
+						DocumentaryUnit.class);
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setNamespaceAware(true); // never forget this!
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(args[0]);
+		return unit;
+	}
 
-        Neo4jGraph graph = new Neo4jGraph(args[1]);
-        EadImporter importer = new EadImporter(new FramedGraph<Neo4jGraph>(
-                graph), 1);
+	public static void main(String[] args) throws Exception {
 
-        XPathFactory xfactory = XPathFactory.newInstance();
-        XPath xpath = xfactory.newXPath();
-        XPathExpression expr = xpath.compile("//eadlist/ead");
+		if (args.length != 3)
+			throw new RuntimeException(
+					"Usage: importer <agent-id> <ead.xml> <neo4j-graph-dir>");
 
-        NodeList result = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-        
-        
-        try {
-            graph.getRawGraph().beginTx();
-            for (int i = 0; i < result.getLength(); i++) {
-                importer.importDocumentaryUnit(result.item(i));
-            }
-            graph.stopTransaction(Conclusion.SUCCESS);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            graph.stopTransaction(Conclusion.FAILURE);            
-        } finally {
-            graph.shutdown();
-        }
-    }
+		// Get the graph and search it for the required agent...
+		FramedGraph<Neo4jGraph> graph = new FramedGraph<Neo4jGraph>(
+				new Neo4jGraph(args[2]));
+		Agent agent = graph.getVertices("identifier", args[0], Agent.class)
+				.iterator().next();
+
+		// XML parsing boilerplate...
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document doc = builder.parse(args[1]);
+
+		// Extract the EAD instances from the eadlist.
+		XPathFactory xfactory = XPathFactory.newInstance();
+		XPath xpath = xfactory.newXPath();
+		XPathExpression expr = xpath.compile("//eadlist/ead");
+
+		NodeList result = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+		try {
+			// Initialize the importer...
+			EadImporter importer = new EadImporter(graph, agent);
+
+			graph.getBaseGraph().getRawGraph().beginTx();
+			for (int i = 0; i < result.getLength(); i++) {
+				importer.importDocumentaryUnit(result.item(i));
+			}
+			graph.getBaseGraph().stopTransaction(Conclusion.SUCCESS);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			graph.getBaseGraph().stopTransaction(Conclusion.FAILURE);
+		} finally {
+			graph.shutdown();
+		}
+	}
 }
