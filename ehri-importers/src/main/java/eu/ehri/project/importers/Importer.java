cfc557b17b3e02147bded305619960323647be96
Mike Bryant <mikesname@gmail.com>
2012-09-28 13:29:00 +0100

    Handle certain errors more smartly, and give the import manager a tolerant mode where it will simply skip items that don't conform. If no items are created in a batch, the manager will throw a NoItemsCreated exception, which will trigger a rollback of the action.

--- ehri-importers/src/main/java/eu/ehri/project/importers/Importer.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/Importer.java
@@ -1,9 +1,10 @@
 package eu.ehri.project.importers;
 
 import eu.ehri.project.exceptions.ValidationError;
+import eu.ehri.project.importers.exceptions.InvalidInputDataError;
 
 public interface Importer<T> {
-    abstract void importItems() throws ValidationError;
+    abstract void importItems() throws ValidationError, InvalidInputDataError;
 
     public void addCreationCallback(final CreationCallback cb);
 }
