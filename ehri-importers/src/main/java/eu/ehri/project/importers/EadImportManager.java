baa9cbdc04d5faae76988fae0c6cb4fff73304c5
Mike Bryant <mikesname@gmail.com>
2012-09-28 16:57:14 +0100

    Initial work on command line support classes.

--- ehri-importers/src/main/java/eu/ehri/project/importers/EadImportManager.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/EadImportManager.java
@@ -3,11 +3,7 @@ package eu.ehri.project.importers;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,11 +12,6 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.PosixParser;
 import org.neo4j.graphdb.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,13 +29,9 @@ import eu.ehri.project.importers.exceptions.InvalidInputDataError;
 import eu.ehri.project.importers.exceptions.NoItemsCreated;
 import eu.ehri.project.models.Action;
 import eu.ehri.project.models.Agent;
-import eu.ehri.project.models.UserProfile;
 import eu.ehri.project.models.base.AccessibleEntity;
 import eu.ehri.project.models.base.Actioner;
 import eu.ehri.project.persistance.ActionManager;
-import eu.ehri.project.persistance.BundleDAO;
-import eu.ehri.project.persistance.BundleFactory;
-import eu.ehri.project.persistance.EntityBundle;
 
 /**
  * Class that provides a front-end for importing EAD XML files, EADGRP, and
@@ -306,106 +293,5 @@ public class EadImportManager extends XmlImportManager implements ImportManager 
                 throw e;
         }
         return counter.count;
-    }
-
-    /**
-     * Command-line entry-point (for testing.)
-     * 
-     * @param args
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception {
-
-        final String logMessage = "Imported from command-line";
-
-        Options options = new Options();
-        options.addOption(new Option("createrepo", false,
-                "Create agent with the given ID"));
-        options.addOption(new Option("repo", true,
-                "Identifier of repository to import into"));
-        options.addOption(new Option("createuser", false,
-                "Create user with the given ID"));
-        options.addOption(new Option("user", true,
-                "Identifier of user to import as"));
-        options.addOption(new Option("tolerant", false,
-                "Don't error if a file is not valid."));
-
-        CommandLineParser parser = new PosixParser();
-        CommandLine cmdLine = parser.parse(options, args);
-
-        if (cmdLine.getArgList().size() < 1)
-            throw new RuntimeException(
-                    "Usage: importer [OPTIONS] -user <user-id> -repo <agent-id> <neo4j-graph-dir> <ead1.xml> <ead2.xml> ... <eadN.xml>");
-
-        // Get the graph and search it for the required agent...
-        FramedGraph<Neo4jGraph> graph = new FramedGraph<Neo4jGraph>(
-                new Neo4jGraph((String) cmdLine.getArgList().get(0)));
-
-        List<String> filePaths = new LinkedList<String>();
-        for (int i = 1; i < cmdLine.getArgList().size(); i++) {
-            filePaths.add((String) cmdLine.getArgList().get(i));
-        }
-
-        Transaction tx = graph.getBaseGraph().getRawGraph().beginTx();
-        try {
-            Agent agent = null;
-            if (cmdLine.hasOption("createrepo")) {
-                agent = createAgent(graph, cmdLine.getOptionValue("repo"));
-            } else {
-                agent = graph
-                        .getVertices("identifier",
-                                (String) cmdLine.getArgList().get(0),
-                                Agent.class).iterator().next();
-            }
-            UserProfile user = null;
-            if (cmdLine.hasOption("createuser")) {
-                user = createUser(graph, cmdLine.getOptionValue("user"));
-            } else {
-                user = graph
-                        .getVertices("identifier",
-                                (String) cmdLine.getArgList().get(0),
-                                UserProfile.class).iterator().next();
-            }
-
-            EadImportManager manager = new EadImportManager(graph, agent, user);
-            manager.setTolerant(cmdLine.hasOption("tolerant"));
-            Action action = manager.importFiles(filePaths, logMessage);
-
-            int itemCount = 0;
-            for (@SuppressWarnings("unused")
-            AccessibleEntity ent : action.getSubjects()) {
-                itemCount++;
-            }
-
-            System.out.println("Imported item count: " + itemCount);
-
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            tx.failure();
-        } finally {
-            tx.finish();
-            graph.shutdown();
-        }
-    }
-
-    private static UserProfile createUser(FramedGraph<Neo4jGraph> graph,
-            String name) throws ValidationError {
-        Map<String, Object> agentData = new HashMap<String, Object>();
-        agentData.put("userId", name);
-        agentData.put("name", name);
-        EntityBundle<UserProfile> agb = new BundleFactory<UserProfile>()
-                .buildBundle(agentData, UserProfile.class);
-        return new BundleDAO<UserProfile>(graph).create(agb);
-    }
-
-    private static Agent createAgent(FramedGraph<Neo4jGraph> graph, String name)
-            throws ValidationError {
-        Map<String, Object> agentData = new HashMap<String, Object>();
-        agentData.put("identifier", name);
-        agentData.put("name", name);
-        EntityBundle<Agent> agb = new BundleFactory<Agent>().buildBundle(
-                agentData, Agent.class);
-        return new BundleDAO<Agent>(graph).create(agb);
     }
 }
--- scripts/importEad.sh
+++ scripts/importEad.sh
@@ -6,6 +6,6 @@ ARGS="$@"
 
 mvn -Dexec.classpathScope=test \
     -pl ehri-importers \
-    -Dexec.mainClass="eu.ehri.project.importers.EadImportManager" \
+    -Dexec.mainClass="eu.ehri.project.commands.CmdEntryPoint" \
     -Dexec.args="$ARGS" \
     exec:java
