e312ce6d00b935ed16ddf62de29d68e5ad99f2e6
Mike Bryant <mikesname@gmail.com>
2012-09-28 14:16:40 +0100

    Add tests for tolerant behaviour and transactional rollback.

--- ehri-importers/src/test/java/eu/ehri/project/importers/test/InvalidEadImporterTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/test/InvalidEadImporterTest.java
@@ -1,5 +1,7 @@
 package eu.ehri.project.importers.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,6 +10,7 @@ import org.xml.sax.SAXException;
 
 import eu.ehri.project.exceptions.ValidationError;
 import eu.ehri.project.importers.EadImportManager;
+import eu.ehri.project.importers.exceptions.NoItemsCreated;
 import eu.ehri.project.models.Action;
 import eu.ehri.project.models.Agent;
 import eu.ehri.project.models.UserProfile;
@@ -24,7 +27,7 @@ public class InvalidEadImporterTest extends AbstractFixtureTest {
     protected final String IMPORTED_ITEM_ID = "C00001";
 
     @Test(expected=ValidationError.class)
-    public void testImportItemsT() throws ValidationError, SAXException, IOException {
+    public void testImportItemsT() throws ValidationError, SAXException, IOException, NoItemsCreated {
         UserProfile user = graph.frame(graph.getVertex(validUserId),
                 UserProfile.class);
         Agent agent = graph.frame(helper.getTestVertex(TEST_REPO), Agent.class);
@@ -41,6 +44,46 @@ public class InvalidEadImporterTest extends AbstractFixtureTest {
         }
     }
 
+    @Test(expected=NoItemsCreated.class)
+    public void testTolerantImport() throws ValidationError, SAXException, IOException, NoItemsCreated {
+        UserProfile user = graph.frame(graph.getVertex(validUserId),
+                UserProfile.class);
+        Agent agent = graph.frame(helper.getTestVertex(TEST_REPO), Agent.class);
+        final String logMessage = "Importing an invalid EAD";
+
+        EadImportManager manager = new EadImportManager(graph, agent, user);
+        manager.setTolerant(true);        
+        InputStream ios = ClassLoader.getSystemResourceAsStream(INVALID_EAD);
+        try {            
+            manager.importFile(logMessage, ios);
+        } finally {
+            ios.close();
+        }
+    }
+
+    @Test(expected=NoItemsCreated.class)
+    public void testRollback() throws ValidationError, SAXException, IOException, NoItemsCreated {
+        UserProfile user = graph.frame(graph.getVertex(validUserId),
+                UserProfile.class);
+        Agent agent = graph.frame(helper.getTestVertex(TEST_REPO), Agent.class);
+        final String logMessage = "Importing an invalid EAD";
+
+        int count = getNodeCount();
+        EadImportManager manager = new EadImportManager(graph, agent, user);
+        manager.setTolerant(true);        
+        InputStream ios = ClassLoader.getSystemResourceAsStream(INVALID_EAD);
+        try {            
+            manager.importFile(logMessage, ios);
+        } catch (ValidationError e) {
+            // we knew this would happen
+        } finally {
+            ios.close();
+        }
+        // Check the state of the graph is the same...
+        assertEquals(count, getNodeCount());
+    }
+    
+    
     private int getNodeCount() {
         // Note: deprecated use of getAllNodes...
         return toList(graph.getBaseGraph().getRawGraph().getAllNodes()).size();
