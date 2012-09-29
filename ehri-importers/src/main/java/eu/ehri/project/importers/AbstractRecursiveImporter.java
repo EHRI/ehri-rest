22aadc02acd06b88ae3a4a86350a078e3c814447
Mike Bryant <mikesname@gmail.com>
2012-09-29 12:06:37 +0100

    Format file

--- ehri-importers/src/main/java/eu/ehri/project/importers/AbstractRecursiveImporter.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/AbstractRecursiveImporter.java
@@ -30,192 +30,192 @@ import eu.ehri.project.persistance.EntityBundle;
  * @param <T>
  */
 public abstract class AbstractRecursiveImporter<T> implements Importer<T> {
-	private static final String IDENTITY_KEY = "identifier";
-	private Agent repository;
-	private FramedGraph<Neo4jGraph> framedGraph;
+    private static final String IDENTITY_KEY = "identifier";
+    private Agent repository;
+    private FramedGraph<Neo4jGraph> framedGraph;
 
-	private List<CreationCallback> createCallbacks = new LinkedList<CreationCallback>();
-	private List<CreationCallback> updateCallbacks = new LinkedList<CreationCallback>();
+    private List<CreationCallback> createCallbacks = new LinkedList<CreationCallback>();
+    private List<CreationCallback> updateCallbacks = new LinkedList<CreationCallback>();
 
-	public AbstractRecursiveImporter(FramedGraph<Neo4jGraph> framedGraph,
-			Agent repository) {
-		this.repository = repository;
-		this.framedGraph = framedGraph;
-	}
+    public AbstractRecursiveImporter(FramedGraph<Neo4jGraph> framedGraph,
+            Agent repository) {
+        this.repository = repository;
+        this.framedGraph = framedGraph;
+    }
 
-	/**
-	 * Add a callback to run when an item is created.
-	 * 
-	 * @param cb
-	 */
-	public void addCreationCallback(final CreationCallback cb) {
-		createCallbacks.add(cb);
-	}
+    /**
+     * Add a callback to run when an item is created.
+     * 
+     * @param cb
+     */
+    public void addCreationCallback(final CreationCallback cb) {
+        createCallbacks.add(cb);
+    }
 
-	public void addUpdateCallback(final CreationCallback cb) {
-		updateCallbacks.add(cb);
-	}
+    public void addUpdateCallback(final CreationCallback cb) {
+        updateCallbacks.add(cb);
+    }
 
-	/**
-	 * Extract child items.
-	 * 
-	 * @param data
-	 * @return
-	 */
-	public List<T> extractChildData(T data) {
-		return new LinkedList<T>();
-	}
+    /**
+     * Extract child items.
+     * 
+     * @param data
+     * @return
+     */
+    public List<T> extractChildData(T data) {
+        return new LinkedList<T>();
+    }
 
-	/**
-	 * Extract the logical DocumentaryUnit at a given depth.
-	 * 
-	 * @param data
-	 * @param depth
-	 * @return
-	 * @throws ValidationError
-	 */
-	protected EntityBundle<DocumentaryUnit> extractDocumentaryUnit(T data,
-			int depth) throws ValidationError {
+    /**
+     * Extract the logical DocumentaryUnit at a given depth.
+     * 
+     * @param data
+     * @param depth
+     * @return
+     * @throws ValidationError
+     */
+    protected EntityBundle<DocumentaryUnit> extractDocumentaryUnit(T data,
+            int depth) throws ValidationError {
 
-		EntityBundle<DocumentaryUnit> bundle = new BundleFactory<DocumentaryUnit>()
-				.buildBundle(new HashMap<String, Object>(),
-						DocumentaryUnit.class);
+        EntityBundle<DocumentaryUnit> bundle = new BundleFactory<DocumentaryUnit>()
+                .buildBundle(new HashMap<String, Object>(),
+                        DocumentaryUnit.class);
 
-		// Extract details for the logical item here
+        // Extract details for the logical item here
 
-		return bundle;
-	}
+        return bundle;
+    }
 
-	/**
-	 * Extract DocumentDescriptions at a given depth from the input data.
-	 * 
-	 * @param data
-	 * @param depth
-	 * @return
-	 * @throws ValidationError
-	 */
-	public abstract List<EntityBundle<DocumentDescription>> extractDocumentDescriptions(
-			T data, int depth) throws ValidationError;
+    /**
+     * Extract DocumentDescriptions at a given depth from the input data.
+     * 
+     * @param data
+     * @param depth
+     * @return
+     * @throws ValidationError
+     */
+    public abstract List<EntityBundle<DocumentDescription>> extractDocumentDescriptions(
+            T data, int depth) throws ValidationError;
 
-	public abstract List<EntityBundle<DatePeriod>> extractDates(T data);
+    public abstract List<EntityBundle<DatePeriod>> extractDates(T data);
 
-	/**
-	 * Import a single archdesc or c01-12 item, keeping a reference to the
-	 * hierarchical depth.
-	 * 
-	 * @param data
-	 * @param parent
-	 * @param depth
-	 * @throws ValidationError
-	 */
-	protected void importSingleItemAtDepth(T data, DocumentaryUnit parent,
-			int depth) throws ValidationError {
-		EntityBundle<DocumentaryUnit> unit = extractDocumentaryUnit(data, depth);
-		BundleDAO<DocumentaryUnit> persister = new BundleDAO<DocumentaryUnit>(
-				framedGraph);
+    /**
+     * Import a single archdesc or c01-12 item, keeping a reference to the
+     * hierarchical depth.
+     * 
+     * @param data
+     * @param parent
+     * @param depth
+     * @throws ValidationError
+     */
+    protected void importSingleItemAtDepth(T data, DocumentaryUnit parent,
+            int depth) throws ValidationError {
+        EntityBundle<DocumentaryUnit> unit = extractDocumentaryUnit(data, depth);
+        BundleDAO<DocumentaryUnit> persister = new BundleDAO<DocumentaryUnit>(
+                framedGraph);
 
-		// Add dates and descriptions to the bundle since they're @Dependent
-		// relations.
-		for (EntityBundle<DatePeriod> dpb : extractDates(data)) {
-			unit.addRelation(TemporalEntity.HAS_DATE, dpb);
-		}
-		for (EntityBundle<DocumentDescription> dpb : extractDocumentDescriptions(
-				data, depth)) {
-			unit.addRelation(Description.DESCRIBES, dpb);
-		}
+        // Add dates and descriptions to the bundle since they're @Dependent
+        // relations.
+        for (EntityBundle<DatePeriod> dpb : extractDates(data)) {
+            unit.addRelation(TemporalEntity.HAS_DATE, dpb);
+        }
+        for (EntityBundle<DocumentDescription> dpb : extractDocumentDescriptions(
+                data, depth)) {
+            unit.addRelation(Description.DESCRIBES, dpb);
+        }
 
-		Object existingId = getExistingGraphId((String) unit.getData().get(
-				IDENTITY_KEY));
-		DocumentaryUnit frame;
-		if (existingId != null) {
-			frame = persister.update(new EntityBundle<DocumentaryUnit>(
-					existingId, unit.getData(), unit.getBundleClass(), unit
-							.getRelations()));
-		} else {
-			frame = persister.create(unit);
+        Object existingId = getExistingGraphId((String) unit.getData().get(
+                IDENTITY_KEY));
+        DocumentaryUnit frame;
+        if (existingId != null) {
+            frame = persister.update(new EntityBundle<DocumentaryUnit>(
+                    existingId, unit.getData(), unit.getBundleClass(), unit
+                            .getRelations()));
+        } else {
+            frame = persister.create(unit);
 
-			// Set the repository/item relationship
-			repository.addCollection(frame);
-		}
+            // Set the repository/item relationship
+            repository.addCollection(frame);
+        }
 
-		// Set the parent child relationship
-		if (parent != null)
-			parent.addChild(frame);
+        // Set the parent child relationship
+        if (parent != null)
+            parent.addChild(frame);
 
-		// Search through child parts and add them recursively...
-		for (T child : extractChildData(data)) {
-			importMultipleItemsAtDepth(child, frame, depth + 1);
-		}
+        // Search through child parts and add them recursively...
+        for (T child : extractChildData(data)) {
+            importMultipleItemsAtDepth(child, frame, depth + 1);
+        }
 
-		// Run creation callbacks for the new item...
-		if (existingId == null) {
-			for (CreationCallback cb : createCallbacks) {
-				cb.itemImported(frame);
-			}
-		} else {
-			for (CreationCallback cb : updateCallbacks) {
-				cb.itemImported(frame);
-			}
-		}
-	}
+        // Run creation callbacks for the new item...
+        if (existingId == null) {
+            for (CreationCallback cb : createCallbacks) {
+                cb.itemImported(frame);
+            }
+        } else {
+            for (CreationCallback cb : updateCallbacks) {
+                cb.itemImported(frame);
+            }
+        }
+    }
 
-	/**
-	 * Entry point for a top-level DocumentaryUnit item.
-	 */
-	protected void importItem(T data) throws ValidationError {
-		importSingleItemAtDepth(data, null, 0);
-	}
+    /**
+     * Entry point for a top-level DocumentaryUnit item.
+     */
+    protected void importItem(T data) throws ValidationError {
+        importSingleItemAtDepth(data, null, 0);
+    }
 
-	/**
-	 * Import multiple items with a given parent at a given depth.
-	 * 
-	 * @param data
-	 * @param parent
-	 * @param depth
-	 * @throws ValidationError
-	 */
-	protected abstract void importMultipleItemsAtDepth(T data,
-			DocumentaryUnit parent, int depth) throws ValidationError;
+    /**
+     * Import multiple items with a given parent at a given depth.
+     * 
+     * @param data
+     * @param parent
+     * @param depth
+     * @throws ValidationError
+     */
+    protected abstract void importMultipleItemsAtDepth(T data,
+            DocumentaryUnit parent, int depth) throws ValidationError;
 
-	/**
-	 * Import (multiple) items from the top-level data.
-	 * 
-	 * @param data
-	 * @param agent
-	 * @throws ValidationError
-	 */
-	public void importItemsFromData(T data) throws ValidationError {
-		importMultipleItemsAtDepth(data, null, 0);
-	}
+    /**
+     * Import (multiple) items from the top-level data.
+     * 
+     * @param data
+     * @param agent
+     * @throws ValidationError
+     */
+    public void importItemsFromData(T data) throws ValidationError {
+        importMultipleItemsAtDepth(data, null, 0);
+    }
 
-	/**
-	 * Main entry-point to trigger parsing.
-	 * 
-	 * @throws InvalidInputDataError
-	 * 
-	 */
-	public abstract void importItems() throws ValidationError,
-			InvalidInputDataError;
+    /**
+     * Main entry-point to trigger parsing.
+     * 
+     * @throws InvalidInputDataError
+     * 
+     */
+    public abstract void importItems() throws ValidationError,
+            InvalidInputDataError;
 
-	// Helpers.
+    // Helpers.
 
-	/**
-	 * Lookup the graph ID of an existing object based on the IDENTITY_KEY
-	 * 
-	 * @param id
-	 * @return
-	 */
-	private Object getExistingGraphId(final String id) {
-		// Lookup the graph id of an object with the same
-		// identity key...
-		@SuppressWarnings({ "unchecked", "rawtypes" })
-		GremlinPipeline pipe = new GremlinPipeline(repository.asVertex())
-				.out(Agent.HOLDS).filter(new PipeFunction<Vertex, Boolean>() {
-					public Boolean compute(Vertex item) {
-						String vid = (String) item.getProperty(IDENTITY_KEY);
-						return (vid != null && vid.equals(id));
-					}
-				}).id();
-		return pipe.hasNext() ? pipe.next() : null;
-	}
+    /**
+     * Lookup the graph ID of an existing object based on the IDENTITY_KEY
+     * 
+     * @param id
+     * @return
+     */
+    private Object getExistingGraphId(final String id) {
+        // Lookup the graph id of an object with the same
+        // identity key...
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        GremlinPipeline pipe = new GremlinPipeline(repository.asVertex())
+                .out(Agent.HOLDS).filter(new PipeFunction<Vertex, Boolean>() {
+                    public Boolean compute(Vertex item) {
+                        String vid = (String) item.getProperty(IDENTITY_KEY);
+                        return (vid != null && vid.equals(id));
+                    }
+                }).id();
+        return pipe.hasNext() ? pipe.next() : null;
+    }
 }
