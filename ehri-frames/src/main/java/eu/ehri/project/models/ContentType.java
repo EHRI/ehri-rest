46f9089ef4ab93006219fb3caa38a50f053087e2
Mike Bryant <mikesname@gmail.com>
2012-10-28 19:46:36 +0000

    Cleanup

--- ehri-frames/src/main/java/eu/ehri/project/models/ContentType.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/ContentType.java
@@ -2,7 +2,6 @@ package eu.ehri.project.models;
 
 import eu.ehri.project.models.annotations.EntityType;
 import eu.ehri.project.models.base.AccessibleEntity;
-import eu.ehri.project.models.base.PermissionGrantTarget;
 
 @EntityType(EntityTypes.CONTENT_TYPE)
 public interface ContentType extends AccessibleEntity {
