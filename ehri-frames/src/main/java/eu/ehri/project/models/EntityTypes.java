f50f127099d6423660e79186b66305b1c08bb2d9
Mike Bryant <mikesname@gmail.com>
2012-11-08 10:51:42 +0000

    Restore accidentally zapped file

---
+++ ehri-frames/src/main/java/eu/ehri/project/models/EntityTypes.java
@@ -0,0 +1,25 @@
+package eu.ehri.project.models;
+
+/*
+ * Set of entity types. TODO: Perhaps convert this to an enum?
+ */
+public class EntityTypes {
+    public static final String ACTION = "action";
+    public static final String AGENT = "agent";
+    public static final String AUTHORITY = "authority";
+    public static final String DOCUMENTARY_UNIT = "documentaryUnit";
+    public static final String DOCUMENT_DESCRIPTION = "documentDescription";
+    public static final String AGENT_DESCRIPTION = "agentDescription";
+    public static final String AUTHORITY_DESCRIPTION = "authorityDescription";
+    public static final String GROUP = "group";
+    public static final String USER_PROFILE = "userProfile";
+    public static final String DATE_PERIOD = "datePeriod";
+    public static final String ANNOTATION = "annotation";
+    public static final String ADDRESS = "address";
+    public static final String IMPORT = "import";
+    public static final String PROPERTY = "property";
+    public static final String PERMISSION = "permission";
+    public static final String PERMISSION_GRANT = "permissionGrant";
+    public static final String CONTENT_TYPE = "contentType";
+    
+}
