9ccaf0eab3617af7c61371f01d01f1157e066938
Mike Bryant <mikesname@gmail.com>
2012-11-12 14:30:34 +0000

    Expose the base url

--- ehri-extension/src/test/java/eu/ehri/extension/test/BaseRestClientTest.java
+++ ehri-extension/src/test/java/eu/ehri/extension/test/BaseRestClientTest.java
@@ -37,6 +37,10 @@ public class BaseRestClientTest extends AbstractRestClientTest {
 
     protected static ServerRunner runner;
 
+    protected String getBaseUri() {
+        return baseUri;
+    }
+    
     @Override
     String getExtensionEntryPointUri() {
         return extensionEntryPointUri;
