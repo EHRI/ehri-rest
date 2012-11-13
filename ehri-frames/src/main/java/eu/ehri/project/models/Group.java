a30c880f8c5cec9c9dc13d16a83e60cadbcc64db
Mike Bryant <mikesname@gmail.com>
2012-11-13 17:36:11 +0000

    Make group membership of other groups a @Fetched property

--- ehri-frames/src/main/java/eu/ehri/project/models/Group.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/Group.java
@@ -6,6 +6,7 @@ import com.tinkerpop.frames.Property;
 import com.tinkerpop.frames.VertexFrame;
 
 import eu.ehri.project.models.annotations.EntityType;
+import eu.ehri.project.models.annotations.Fetch;
 import eu.ehri.project.models.base.AccessibleEntity;
 import eu.ehri.project.models.base.Accessor;
 import eu.ehri.project.models.base.PermissionScope;
@@ -19,6 +20,10 @@ public interface Group extends VertexFrame, Accessor, AccessibleEntity,
     public static final String ADMIN_GROUP_IDENTIFIER = "admin";
     public static final String ANONYMOUS_GROUP_IDENTIFIER = "anonymous";
 
+    @Fetch
+    @Adjacency(label = BELONGS_TO)
+    public Iterable<Group> getGroups();
+    
     @Adjacency(label = BELONGS_TO, direction = Direction.IN)
     public Iterable<Accessor> getMembers();
 
