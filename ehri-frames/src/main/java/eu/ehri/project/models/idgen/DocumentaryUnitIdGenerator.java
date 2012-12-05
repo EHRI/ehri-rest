e46ea299245b05f640e9b41f512af9153ae2113b
Mike Bryant <mikesname@gmail.com>
2012-12-05 14:57:54 +0000

    Add more checks to DocumentaryUnit ID generator.

--- ehri-frames/src/main/java/eu/ehri/project/models/idgen/DocumentaryUnitIdGenerator.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/idgen/DocumentaryUnitIdGenerator.java
@@ -18,11 +18,17 @@ public class DocumentaryUnitIdGenerator implements IdGenerator {
     public String generateId(String entityTypePrefix, PermissionScope scope,
             Map<String, Object> data) throws IdGenerationError {
         if (scope == null)
-            throw new IdGenerationError("Null or incorrect scope given for ID generation",
+            throw new IdGenerationError(
+                    "Null or incorrect scope given for ID generation",
                     entityTypePrefix, scope, data);
-        return entityTypePrefix + SEPERATOR
-                + scope.getIdentifier()
-                + SEPERATOR + data.get(AccessibleEntity.IDENTIFIER_KEY);
+        String ident = (String) data.get(AccessibleEntity.IDENTIFIER_KEY);
+        if (ident == null || ident.trim().isEmpty()) {
+            throw new IdGenerationError(
+                    "Documentary unit 'identifier' field was missing.",
+                    entityTypePrefix, scope, data);
+        }
+        return entityTypePrefix + SEPERATOR + scope.getIdentifier() + SEPERATOR
+                + ident;
     }
 
 }
