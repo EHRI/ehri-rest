5f9481688026f67a2bb7281ecbe77c65b931d505
Mike Bryant <mikesname@gmail.com>
2012-12-17 17:23:34 +0000

    Add Guava to deps

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@
 
 BLUEPRINTS_VERS="2.1.0"
 BLUEPRINTS_DEPS=( frames blueprints-core blueprints-neo4j-graph pipes )
-EXTRA_DEP=( joda-time-2.1.jar )
+EXTRA_DEPS=( guava-10.0.1.jar joda-time-2.1.jar )
 
 if [ $# -ne 1 ]; then
     echo "Usage: `basename $0` <NEO4JPATH>"
