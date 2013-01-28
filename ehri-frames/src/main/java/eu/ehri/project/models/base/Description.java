df23f62fe170680ccdc3af90e7a8048b8e259fbb
Mike Bryant <mikesname@gmail.com>
2013-01-28 15:57:39 +0000

    Serialize Description entity at top-level only

--- ehri-frames/src/main/java/eu/ehri/project/models/base/Description.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/base/Description.java
@@ -4,6 +4,8 @@ import com.tinkerpop.frames.Adjacency;
 import com.tinkerpop.frames.Property;
 import com.tinkerpop.frames.VertexFrame;
 
+import eu.ehri.project.models.annotations.Fetch;
+
 public interface Description extends VertexFrame, VersionedEntity {
     public static final String DESCRIBES = "describes";
     public static final String LANGUAGE_CODE = "languageCode";
@@ -14,4 +16,14 @@ public interface Description extends VertexFrame, VersionedEntity {
     @Property(LANGUAGE_CODE)
     public String getLanguageOfDescription();
     
+    /**
+     * Get the described entity of a description. This 
+     * method if @Fetch serialized only if the description
+     * is at the top level of the requested subtree.
+     * 
+     * @return
+     */
+    @Fetch(ifDepth=0)
+    @Adjacency(label = DESCRIBES)
+    public DescribedEntity getDescribedEntity();    
 }
