5e0bbd5db00f0e3078aa2b128ab393ef8eee4c5b
Mike Bryant <mikesname@gmail.com>
2013-03-04 23:05:25 +0000

    Remove unused cruft

--- ehri-frames/src/main/java/eu/ehri/project/models/base/VersionedEntity.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/base/VersionedEntity.java
@@ -1,19 +1,13 @@
 package eu.ehri.project.models.base;
 
 import com.tinkerpop.frames.Adjacency;
-import com.tinkerpop.frames.Incidence;
 import com.tinkerpop.frames.VertexFrame;
 
 import eu.ehri.project.models.Revision;
-import eu.ehri.project.relationships.RevisionContext;
 
 public interface VersionedEntity extends VertexFrame {
     public static final String HAS_REVISION = "hasRevision";
 
     @Adjacency(label = HAS_REVISION)
     public Iterable<Revision> getRevisions();
-
-    @Incidence(label = HAS_REVISION)
-    public Iterable<RevisionContext> getRevisionContexts();
-
 }
