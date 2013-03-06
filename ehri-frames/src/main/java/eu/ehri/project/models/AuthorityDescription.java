8cc20c63e297b2206bb88688c7da92c9e6a7cfde
Mike Bryant <mikesname@gmail.com>
2013-03-06 13:19:02 +0000

    Add the REST interface for the Authority/CPF (Isaar) model. Also made some alterations to the model itself so that the entity type is part of the description - it's plausable we'll want to omit this for placeholder items. Most CRUD and tests copy/pasted from Repository resource.

--- ehri-frames/src/main/java/eu/ehri/project/models/AuthorityDescription.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/AuthorityDescription.java
@@ -9,6 +9,13 @@ import eu.ehri.project.models.base.Description;
 @EntityType(EntityClass.AUTHORITY_DESCRIPTION)
 public interface AuthorityDescription extends VertexFrame, Description {
 
-    @Property("title")
-    public String getTitle();
+    public static final String AUTHORITY_NAME = "name"; // Authorized form of name
+    public static final String AUTHORITY_TYPE = "typeOfEntity";
+
+    @Property(AUTHORITY_NAME)
+    public String getName();
+
+    @Property(AUTHORITY_TYPE)
+    public String getTypeOfEntity();
+
 }
