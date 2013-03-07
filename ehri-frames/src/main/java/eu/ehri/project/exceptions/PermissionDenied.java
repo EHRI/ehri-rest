b68dd536182b6905845998fa53bd385b2456dcc0
Mike Bryant <mikesname@gmail.com>
2013-03-07 14:41:21 +0000

    Partial cleanup of still-horrid permission constructors/throwing args

--- ehri-frames/src/main/java/eu/ehri/project/exceptions/PermissionDenied.java
+++ ehri-frames/src/main/java/eu/ehri/project/exceptions/PermissionDenied.java
@@ -27,7 +27,7 @@ public class PermissionDenied extends Exception {
     public PermissionDenied(String accessor, String entity, String message) {
         super(String.format(
                 "Permission denied accessing resource '%s' as '%s': %s",
-                entity, accessor));
+                entity, accessor, entity, message));
         this.accessor = accessor;
         this.entity = entity;
     }
--- ehri-frames/src/main/java/eu/ehri/project/views/ViewHelper.java
+++ ehri-frames/src/main/java/eu/ehri/project/views/ViewHelper.java
@@ -111,7 +111,7 @@ public final class ViewHelper {
     public void checkReadAccess(AccessibleEntity entity, Accessor user)
             throws PermissionDenied {
         if (!acl.getAccessControl(entity, user))
-            throw new PermissionDenied(user.getIdentifier(), manager.getId(entity));
+            throw new PermissionDenied(user.getIdentifier(), manager.getId(entity), "Restricted access");
     }
 
     /**
