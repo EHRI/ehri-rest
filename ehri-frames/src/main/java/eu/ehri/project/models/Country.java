8e6ff92f24831968f4b21a7d33f9b6f861d6681b
Mike Bryant <mikesname@gmail.com>
2013-05-01 12:57:29 +0100

    Don't make country a described entity just yet - we don't need it.

--- ehri-frames/src/main/java/eu/ehri/project/models/Country.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/Country.java
@@ -4,7 +4,7 @@ import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.frames.Adjacency;
 import com.tinkerpop.frames.Property;
 import eu.ehri.project.models.annotations.EntityType;
-import eu.ehri.project.models.base.DescribedEntity;
+import eu.ehri.project.models.base.AccessibleEntity;
 import eu.ehri.project.models.base.IdentifiableEntity;
 import eu.ehri.project.models.base.PermissionScope;
 
@@ -15,7 +15,7 @@ import eu.ehri.project.models.base.PermissionScope;
  * be represented by an ISO3166 Alpha 2 code, lower cased.
  */
 @EntityType(EntityClass.COUNTRY)
-public interface Country extends DescribedEntity, PermissionScope {
+public interface Country extends IdentifiableEntity, AccessibleEntity, PermissionScope {
 
     public static final String COUNTRY_CODE = IdentifiableEntity.IDENTIFIER_KEY;
 
