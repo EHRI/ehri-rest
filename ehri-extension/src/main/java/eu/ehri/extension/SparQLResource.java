3b342cd7a7264d4f2e136bbafa781cfc7ba6742d
Mike Bryant <mikesname@gmail.com>
2013-06-11 09:56:24 +0100

    Clean up Sparql resource

--- ehri-extension/src/main/java/eu/ehri/extension/SparQLResource.java
+++ ehri-extension/src/main/java/eu/ehri/extension/SparQLResource.java
@@ -45,15 +45,14 @@ import java.util.Map;
  * database.
  * <p/>
  * * Insecure and SHOULD NOT be a public endpoint
- * * Ported from Blueprints and Neo4j plugins
  */
 @Path("sparql")
 public class SparQLResource extends AbstractRestResource {
 
     private PropertyGraphSail sail;
-    private QueryParser parser;
-    private SailRepositoryConnection sc;
-    private SailRepository repo;
+    private final QueryParser parser = new SPARQLParser();
+    private final JsonFactory factory = new JsonFactory();
+    private final ObjectMapper mapper = new ObjectMapper(factory);
 
     /**
      * @param database Injected neo4j database
@@ -63,60 +62,57 @@ public class SparQLResource extends AbstractRestResource {
 
     }
 
+    /**
+     * Run a sparql query.
+     * @param queryString
+     * @return
+     * @throws Exception
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/")
     public Response sparqlQuery(
             @DefaultValue("") @QueryParam("q") String queryString) throws Exception {
-        System.out.println("SPARQL: " + queryString);
+        System.out.println("SparQL Query: " + queryString);
+
         initSail();
-        //test();
+        ParsedQuery query = parser.parseQuery(queryString, "http://ehri-project.eu");
+        CloseableIteration<? extends BindingSet, QueryEvaluationException> results
+                = sail.getConnection().evaluate(
+                    query.getTupleExpr(), query.getDataset(), new EmptyBindingSet(), false);
         try {
-
-            /*ParsedQuery query = parser.parseQuery(queryString, "http://ehri-project.eu");
-            BindingSet bindings = new EmptyBindingSet();
-            CloseableIteration<? extends BindingSet, QueryEvaluationException> sparqlResults
-                    = sail.getConnection().evaluate(
-                    query.getTupleExpr(), query.getDataset(),
-                    bindings, false);*/
-
-            /*test2(queryString);
-
-
-            SailTupleQuery tupleQuery = repo.getConnection().prepareTupleQuery(
-                    QueryLanguage.SPARQL, queryString, "http://example.org/bogus");
-            TupleQueryResult result = tupleQuery.evaluate();*/
-
-            ParsedQuery query = new SPARQLParser().parseQuery(queryString, "http://example.org/bogus/");
-            CloseableIteration<? extends BindingSet, QueryEvaluationException> results
-                    = sail.getConnection().evaluate(query.getTupleExpr(), query.getDataset(), new EmptyBindingSet(), false);
-            try {
-                List<Map<String,String>> out = Lists.newArrayList();
-                while (results.hasNext()) {
-                    BindingSet next = results.next();
-                    Map<String,String> set = Maps.newHashMap();
-                    for (String name : next.getBindingNames()) {
-                        System.out.println("key = " + name + ", value = " + next.getValue(name).stringValue());
-                        set.put(name, next.getValue(name).stringValue());
-                    }
-                    out.add(set);
+            List<Map<String,String>> out = Lists.newArrayList();
+            while (results.hasNext()) {
+                BindingSet next = results.next();
+                Map<String,String> set = Maps.newHashMap();
+                for (String name : next.getBindingNames()) {
+                    set.put(name, next.getValue(name).stringValue());
                 }
-                JsonFactory factory = new JsonFactory();
-                ObjectMapper mapper = new ObjectMapper(factory);
-                TypeReference<List<Map<String,String>>> typeRef = new TypeReference<List<Map<String,String>>>() {
-                };
-
-                return Response.ok(mapper.writeValueAsBytes(out)).build();
-            } finally {
-                results.close();
+                out.add(set);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new WebApplicationException(e);
+            return Response.ok(mapper.writeValueAsBytes(out)).build();
+        } finally {
+            results.close();
         }
     }
 
-    private void test() throws Exception {
+    /**
+     * Initialise the PropertySailGraph
+     */
+    private void initSail() {
+        if (sail == null) {
+            sail = new PropertyGraphSail(graph.getBaseGraph());
+            try {
+                sail.initialize();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new WebApplicationException(e);
+            }
+        }
+    }
+
+    // TODO: Make me a test
+    private void testSparql() throws Exception {
         String queryStr = "PREFIX pgm: <" + PropertyGraphSail.ONTOLOGY_NS + ">\n" +
                 "PREFIX prop: <" + PropertyGraphSail.PROPERTY_NS + ">\n" +
                 "SELECT ?project ?name WHERE {\n" +
@@ -135,15 +131,6 @@ public class SparQLResource extends AbstractRestResource {
         sail.initialize();
 
         SailRepository repo = new SailRepository(sail);
-
-/*
-        System.out.println(queryStr);
-        ParsedQuery query = new SPARQLParser().parseQuery(queryStr, "http://example.org/bogus/");
-        CloseableIteration<? extends BindingSet, QueryEvaluationException> results
-                = sail.getConnection()
-                .evaluate(query.getTupleExpr(), query.getDataset(), new EmptyBindingSet(), false);
-*/
-
         SailTupleQuery tupleQuery = repo.getConnection()
                 .prepareTupleQuery(QueryLanguage.SPARQL, queryStr, "http://example.org/bogus");
         TupleQueryResult result = tupleQuery.evaluate();
@@ -158,67 +145,5 @@ public class SparQLResource extends AbstractRestResource {
         } finally {
             result.close();
         }
-    }
-
-    private void test2(String queryStr) throws Exception {
-        ParsedQuery query = new SPARQLParser().parseQuery(queryStr, "http://example.org/bogus/");
-        CloseableIteration<? extends BindingSet, QueryEvaluationException> results
-                = sail.getConnection().evaluate(query.getTupleExpr(), query.getDataset(), new EmptyBindingSet(), false);
-        try {
-            while (results.hasNext()) {
-                BindingSet set = results.next();
-                URI project = (URI) set.getValue("project");
-                Literal name = (Literal) set.getValue("name");
-                System.out.println("project = " + project + ", name = " + name);
-            }
-        } finally {
-            results.close();
-        }
-    }
-
-
-    private void initSail() {
-        if (sail == null) {
-            sail = new PropertyGraphSail(graph.getBaseGraph());
-            //sail = new PropertyGraphSail(TinkerGraphFactory.createTinkerGraph());
-            try {
-                sail.initialize();
-                parser = new SPARQLParserFactory().getParser();
-                repo = new SailRepository(sail);
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new WebApplicationException(e);
-            }
-        }
-    }
-
-    private StreamingOutput streamingResults(final CloseableIteration iterable) throws Exception{
-        final ObjectMapper mapper = new ObjectMapper();
-        final JsonFactory f = new JsonFactory();
-        return new StreamingOutput() {
-            @Override
-            public void write(OutputStream arg0) throws IOException,
-                    WebApplicationException {
-                JsonGenerator g = f.createJsonGenerator(arg0);
-                g.writeStartArray();
-
-                try {
-                    while (iterable.hasNext()) {
-                        BindingSet next = (BindingSet)iterable.next();
-                        g.writeStartObject();
-                        for (String name : next.getBindingNames()) {
-                            g.writeFieldName(name);
-                            mapper.writeValue(g, next.getValue(name).stringValue());
-                        }
-                        g.writeEndObject();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    throw new RuntimeException(e);
-                }
-                g.writeEndArray();
-                g.close();
-            }
-        };
     }
 }
