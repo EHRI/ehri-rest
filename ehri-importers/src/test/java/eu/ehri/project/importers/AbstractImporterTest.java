d296185941e1f1bc162f8f0582bcafc4631b51cf
Mike Bryant <mikesname@gmail.com>
2013-07-14 09:34:36 +0100

    Clean up concept-related importer tests

--- ehri-importers/src/test/java/eu/ehri/project/importers/AbstractImporterTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/AbstractImporterTest.java
@@ -9,12 +9,17 @@ import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 import com.tinkerpop.frames.FramedGraph;
+import eu.ehri.project.models.base.Description;
 import eu.ehri.project.models.base.IdentifiableEntity;
+import eu.ehri.project.models.cvoc.Concept;
+import eu.ehri.project.models.cvoc.ConceptDescription;
 import eu.ehri.project.test.AbstractFixtureTest;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.tooling.GlobalGraphOperations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.PrintStream;
 
 
 /**
@@ -22,8 +27,7 @@ import org.slf4j.LoggerFactory;
  */
 public class AbstractImporterTest extends AbstractFixtureTest {
     private static final Logger logger = LoggerFactory.getLogger(AbstractImporterTest.class);
-    
-   
+
     protected void printGraph(FramedGraph<?> graph) {
         int vcount = 0;
         for (Vertex v : graph.getVertices()) {
@@ -33,17 +37,52 @@ public class AbstractImporterTest extends AbstractFixtureTest {
                 if (v.getProperty(key) instanceof String[]) {
                     String[] list = (String[]) v.getProperty(key);
                     for (String o : list) {
-                        value += "["+o.toString() + "] ";
+                        value += "[" + o.toString() + "] ";
                     }
                 } else {
                     value = v.getProperty(key).toString();
                 }
                 logger.debug(key + ": " + value);
             }
-//            
+
             for (Edge e : v.getEdges(Direction.OUT)) {
                 logger.debug(e.getLabel());
             }
+        }
+    }
+
+    // Print a Concept Tree from a 'top' concept down into all narrower concepts
+    public void printConceptTree(final PrintStream out, Concept c) {
+        printConceptTree(out, c, 0, "");
+    }
+
+    public void printConceptTree(final PrintStream out, Concept c, int depth, String indent) {
+        if (depth > 100) {
+            out.println("STOP RECURSION, possibly cyclic 'tree'");
+            return;
+        }
+
+        out.print(indent);
+        out.print("[" + c.getIdentifier() + "]");
+        Iterable<Description> descriptions = c.getDescriptions();
+        for (Description d : descriptions) {
+            String lang = d.getLanguageOfDescription();
+            String prefLabel = (String) d.asVertex().getProperty(ConceptDescription.PREFLABEL); // can't use the getPrefLabel() !
+            out.print(", \"" + prefLabel + "\"(" + lang + ")");
+        }
+        // TODO Print related concept ids?
+        for (Concept related : c.getRelatedConcepts()) {
+            out.print("[" + related.getIdentifier() + "]");
+        }
+        for (Concept relatedBy : c.getRelatedByConcepts()) {
+            out.print("[" + relatedBy.getIdentifier() + "]");
+        }
+
+        out.println("");// end of concept
+
+        indent += ".   ";// the '.' improves readability, but the whole printing could be improved
+        for (Concept nc : c.getNarrowerConcepts()) {
+            printConceptTree(out, nc, ++depth, indent); // recursive call
         }
     }
 
@@ -54,7 +93,13 @@ public class AbstractImporterTest extends AbstractFixtureTest {
 
     protected int getNodeCount(FramedGraph<?> graph) {
         // Unsafe cast here
-        GraphDatabaseService svc = ((Neo4jGraph)graph.getBaseGraph()).getRawGraph();
+        GraphDatabaseService svc = ((Neo4jGraph) graph.getBaseGraph()).getRawGraph();
         return toList(GlobalGraphOperations.at(svc).getAllNodes()).size();
+    }
+
+    protected int getEdgeCount(FramedGraph<?> graph) {
+        // Unsafe cast here
+        GraphDatabaseService svc = ((Neo4jGraph) graph.getBaseGraph()).getRawGraph();
+        return toList(GlobalGraphOperations.at(svc).getAllRelationships()).size();
     }
 }
