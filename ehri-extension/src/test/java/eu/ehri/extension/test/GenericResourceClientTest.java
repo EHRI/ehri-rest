f79257244a7c04899abfc9e38a5d1d3b4ef25a4a
Mike Bryant <mikesname@gmail.com>
2013-08-06 15:42:46 +0100

    Update and improve tests for generic REST resources

--- ehri-extension/src/test/java/eu/ehri/extension/test/GenericResourceClientTest.java
+++ ehri-extension/src/test/java/eu/ehri/extension/test/GenericResourceClientTest.java
@@ -27,7 +27,9 @@ import static junit.framework.Assert.assertTrue;
  */
 public class GenericResourceClientTest  extends BaseRestClientTest {
 
-    private static final String ITEM = "cd1";
+    private static final String ITEM1 = "c1";
+    private static final String ITEM2 = "c4";
+    private static final String BAD_ITEM = "cd1";
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
@@ -35,10 +37,10 @@ public class GenericResourceClientTest  extends BaseRestClientTest {
     }
 
     @Test
-    public void getGeneticEntity() throws IOException {
+    public void getMultipleGenericEntities() throws IOException {
         // Create
         WebResource resource = client.resource(getExtensionEntryPointUri()
-                + "/entities?id=" + ITEM);
+                + "/entities?id=" + ITEM1 + "&id=" + ITEM2);
         ClientResponse response = resource
                 .accept(MediaType.APPLICATION_JSON)
                 .type(MediaType.APPLICATION_JSON)
@@ -52,8 +54,45 @@ public class GenericResourceClientTest  extends BaseRestClientTest {
                 JsonNode.class);
         JsonNode idValue = rootNode.path(0).path(Bundle.ID_KEY);
         assertFalse(idValue.isMissingNode());
-        assertEquals(ITEM, idValue.getTextValue());
+        assertEquals(ITEM1, idValue.getTextValue());
         // ensure only one item was returned...
-        assertTrue(rootNode.path(1).isMissingNode());
+        assertFalse(rootNode.path(1).isMissingNode());
+        assertTrue(rootNode.path(2).isMissingNode());
+    }
+
+    @Test
+    public void getSingleGenericEntity() throws IOException {
+        // Create
+        WebResource resource = client.resource(getExtensionEntryPointUri()
+                + "/entities/" + ITEM1);
+        ClientResponse response = resource
+                .accept(MediaType.APPLICATION_JSON)
+                .type(MediaType.APPLICATION_JSON)
+                .header(AbstractRestResource.AUTH_HEADER_NAME,
+                        getAdminUserProfileId())
+                .get(ClientResponse.class);
+        assertEquals(Response.Status.OK.getStatusCode(),
+                response.getStatus());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readValue(response.getEntity(String.class),
+                JsonNode.class);
+        JsonNode idValue = rootNode.path(Bundle.ID_KEY);
+        assertFalse(idValue.isMissingNode());
+        assertEquals(ITEM1, idValue.getTextValue());
+    }
+
+    @Test
+    public void getCannotFetchNonContentTypes() throws IOException {
+        // Create
+        WebResource resource = client.resource(getExtensionEntryPointUri()
+                + "/entities/" + BAD_ITEM);
+        ClientResponse response = resource
+                .accept(MediaType.APPLICATION_JSON)
+                .type(MediaType.APPLICATION_JSON)
+                .header(AbstractRestResource.AUTH_HEADER_NAME,
+                        getAdminUserProfileId())
+                .get(ClientResponse.class);
+        assertEquals(Response.Status.NOT_FOUND.getStatusCode(),
+                response.getStatus());
     }
 }
