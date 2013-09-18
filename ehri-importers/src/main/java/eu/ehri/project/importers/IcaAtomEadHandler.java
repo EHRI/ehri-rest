45acb398ef38a511782a4ec1e5eb9064360faa40
bencomp <ben@companjen.name>
2013-09-18 14:00:24 +0200

    Add CegesomaHandler, reindent IcaAtomEadHandler, update CegesomaTest

--- ehri-importers/src/main/java/eu/ehri/project/importers/IcaAtomEadHandler.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/IcaAtomEadHandler.java
@@ -88,26 +88,23 @@ public class IcaAtomEadHandler extends SaxXmlHandler {
                     if (!currentGraph.containsKey(Ontology.LANGUAGE_OF_DESCRIPTION)) {
                         currentGraph.put(Ontology.LANGUAGE_OF_DESCRIPTION, "en");
                     }
-                if (!currentGraph.containsKey(Ontology.LANGUAGE_OF_DESCRIPTION)) {
-                    currentGraph.put(Ontology.LANGUAGE_OF_DESCRIPTION, "en");
-                }
-                DocumentaryUnit current = (DocumentaryUnit)importer.importItem(currentGraph, depth);
-                logger.debug("importer used: " + importer.getClass());
-                if (depth > 0) { // if not on root level
-                    children[depth - 1].add(current); // add child to parent offspring
-                    //set the parent child relationships by hand, as we don't have the parent Documentary unit yet.
-                    //only when closing a DocUnit, one can set the relationship to its children, but not its parent, as that has not yet been closed.
-                    for (DocumentaryUnit child : children[depth]) {
-                        if (child != null) {
-                            current.addChild(child);
-                            // FIXME: Is this correct??? It should be done automatically
-                            // using the scope of the BundleDAO, but because the actual
-                            // parent doesn't exist, we have to override it and set
-                            // this here...
-                            child.setPermissionScope(current);
-                        }
+                    DocumentaryUnit current = (DocumentaryUnit)importer.importItem(currentGraph, depth);
+                    logger.debug("importer used: " + importer.getClass());
+                    if (depth > 0) { // if not on root level
+                    	children[depth - 1].add(current); // add child to parent offspring
+                    	//set the parent child relationships by hand, as we don't have the parent Documentary unit yet.
+                    	//only when closing a DocUnit, one can set the relationship to its children, but not its parent, as that has not yet been closed.
+                    	for (DocumentaryUnit child : children[depth]) {
+                    		if (child != null) {
+                    			current.addChild(child);
+                    			// FIXME: Is this correct??? It should be done automatically
+                    			// using the scope of the BundleDAO, but because the actual
+                    			// parent doesn't exist, we have to override it and set
+                    			// this here...
+                    			child.setPermissionScope(current);
+                    		}
+                    	}
                     }
-                }
                 } catch (ValidationError ex) {
                     logger.error(ex.getMessage());
                 } finally {
@@ -125,13 +122,13 @@ public class IcaAtomEadHandler extends SaxXmlHandler {
     @Override
     protected boolean needToCreateSubNode(String qName) {
         //child or parent unit:
-        boolean need = childItemPattern.matcher(qName).matches() || qName.equals("archdesc");
-        //controlAccess 
-        String path = getImportantPath(currentPath);
-        if (path != null) {
-            need = need || path.endsWith("Access");
-        }
-        return need;
+    	boolean need = childItemPattern.matcher(qName).matches() || qName.equals("archdesc");
+    	//controlAccess 
+    	String path = getImportantPath(currentPath);
+    	if (path != null) {
+    		need = need || path.endsWith("Access");
+    	}
+    	return need;
     }
 
     @Override
--- ehri-importers/src/test/java/eu/ehri/project/importers/CegesomaTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/CegesomaTest.java
@@ -13,6 +13,7 @@ import eu.ehri.project.models.DocumentaryUnit;
 import eu.ehri.project.models.base.PermissionScope;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -26,9 +27,9 @@ import static org.junit.Assert.*;
 public class CegesomaTest extends AbstractImporterTest{
     
     protected final String TEST_REPO = "r1";
-    protected final String XMLFILE = "Bestand_Nola_ead.pxml";
+    protected final String XMLFILE = "Bestand_Nola_ead-test.pxml";
     protected final String ARCHDESC = "AA 1134",
-            C01 = "",
+            C01 = "cegesomaID1",
             C02_01 = "AA 1134 / 32",
             C02_02 = "AA 1134 / 34";
     DocumentaryUnit archdesc, c1, c2_1, c2_2;
@@ -42,16 +43,18 @@ public class CegesomaTest extends AbstractImporterTest{
 
         origCount = getNodeCount(graph);
         InputStream ios = ClassLoader.getSystemResourceAsStream(XMLFILE);
-        ImportLog log = new SaxImportManager(graph, agent, validUser, BundesarchiveEadImporter.class, BundesarchiveEadHandler.class).importFile(ios, logMessage);
-//        printGraph(graph);
+        ImportLog log = new SaxImportManager(graph, agent, validUser, IcaAtomEadImporter.class, CegesomaEadHandler.class).importFile(ios, logMessage);
+        printGraph(graph);
         // How many new nodes will have been created? We should have
-        // - 9 more DocumentaryUnits (archdesc, 1-7+7)
-       	// - 9 more DocumentDescription
-        // - 2 more DatePeriod
-        // - 1 more UnknownProperties
-        // - 10 more import Event links (9 for every Unit, 1 for the User)
+        // - 5 more DocumentaryUnits (archdesc, 1 c01, 3 c02)
+       	// - 5 more DocumentDescription
+        // - 6 more DatePeriod
+        // - 32 more subjectAccess nodes
+        // - 1 more corporateBodyAccess
+        // - 1 more creatorAccess
+        // - 6 more import Event links (1 for each Unit, 1 for the User)
         // - 1 more import Event
-        int newCount = origCount + 32;
+        int newCount = origCount + 24 + 34;
         assertEquals(newCount, getNodeCount(graph));
         
         archdesc = graph.frame(
@@ -70,17 +73,18 @@ public class CegesomaTest extends AbstractImporterTest{
 
     //test titles
         for(DocumentDescription d : archdesc.getDocumentDescriptions()){
-            assertEquals("Bestände", d.getName());
+            assertEquals("Deelarchief betreffende het actienetwerk Nola (1942-1944)", d.getName());
         }
         for(DocumentDescription desc : c1.getDocumentDescriptions()){
-                assertEquals("Bundesarchiv", desc.getName());
+                assertEquals("Documenten betreffende l'Union nationale de la Résistance", desc.getName());
         }
 //        for(DocumentDescription d : c7_1.getDocumentDescriptions()){
 //            assertEquals("Generalkommandos der Waffen-SS", d.getName());
 //        }
-//        for(DocumentDescription d : c7_2.getDocumentDescriptions()){
-//            assertEquals("I. SS-Panzerkorps\"Leibstandarte\"", d.getName());
-//        }
+        for(DocumentDescription d : c2_2.getDocumentDescriptions()){
+            assertEquals("Wetteksten (U.) S.R.A.", d.getName());
+            assertEquals("item", d.asVertex().getProperty("levelOfDescription"));
+        }
     //test hierarchy
         assertEquals(new Long(1), archdesc.getChildCount());
         for(DocumentaryUnit d : archdesc.getChildren()){
@@ -95,30 +99,36 @@ public class CegesomaTest extends AbstractImporterTest{
 //            assertEquals("fonds", d.asVertex().getProperty("levelOfDescription"));
 //        }
 //    //test dates
-//        for(DocumentDescription d : c7_1.getDocumentDescriptions()){
-//        	// Single date is just a string
-//        	assertEquals("1942-1945", d.asVertex().getProperty("unitDates"));
-//        	for (DatePeriod dp : d.getDatePeriods()){
-//        		assertEquals("1942-01-01", dp.getStartDate());
-//        		assertEquals("1945-12-31", dp.getEndDate());
-//        	}
-//        	// There was only one date+date type, so it must still be around. 
-//        	assertEquals("Bestandslaufzeit", d.asVertex().getProperty("unitDatesTypes"));
-//        }
-//        
-//        // Second fonds has two dates with different types -> list
-//        for(DocumentDescription d : c7_2.getDocumentDescriptions()){
-//        	// unitDates still around?
-//        	assertEquals("1943-1944", d.asVertex().getProperty("unitDates"));
-//        	// start and end dates correctly parsed and setup
-//        	for(DatePeriod dp : d.getDatePeriods()){
-//        		assertEquals("1943-01-01", dp.getStartDate());
-//        		assertEquals("1944-12-31", dp.getEndDate());
-//        	}
-//        	
-//        	// Since there was a list of unitDateTypes, it should now be deleted
-//        	assertNull(d.asVertex().getProperty("unitDatesTypes"));
-//        }
+        for(DocumentDescription d : c2_1.getDocumentDescriptions()){
+        	// Single date is just a string
+        	assertEquals("1944-1948", d.asVertex().getProperty("unitDates"));
+        	for (DatePeriod dp : d.getDatePeriods()){
+        		assertEquals("1944-01-01", dp.getStartDate());
+        		assertEquals("1948-12-31", dp.getEndDate());
+        	}
+        	// There was only one date+date type, so it must still be around. 
+        	assertEquals("inclusive", d.asVertex().getProperty("unitDatesTypes"));
+        }
+        
+        // Fonds has two dates with different types -> list
+        for(DocumentDescription d : archdesc.getDocumentDescriptions()){
+        	// unitDates still around?
+        	List<String> s = (List<String>) d.asVertex().getProperty("unitDates");
+        	assertEquals("1944-1979", s.get(0));
+        	assertEquals("1944-1948", s.get(1));
+        	
+        	// start and end dates correctly parsed and setup
+        	List<DatePeriod> dp = toList(d.getDatePeriods());
+        	assertEquals(2, dp.size());
+        	assertEquals("1944-01-01", dp.get(0).getStartDate());
+        	assertEquals("1979-12-31", dp.get(0).getEndDate());
+        	assertEquals("1944-01-01", dp.get(1).getStartDate());
+        	assertEquals("1948-12-31", dp.get(1).getEndDate());
+        	
+        	
+        	// Since there was a list of unitDateTypes, it should now be deleted
+        	assertNull(d.asVertex().getProperty("unitDatesTypes"));
+        }
         
     }
 }
