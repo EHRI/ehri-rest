f48bb8a3c591ceb9b62aea0372ce3bdf4c7fb095
Mike Bryant <mikesname@gmail.com>
2013-11-24 00:16:09 +0000

    Rename tests so they're picked up by JUnit

--- ehri-importers/src/test/java/eu/ehri/project/importers/IcaAtomMultiLeveledTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/IcaAtomMultiLeveledTest.java
@@ -5,33 +5,37 @@
 package eu.ehri.project.importers;
 
 import com.tinkerpop.blueprints.Vertex;
-import eu.ehri.project.models.Repository;
 import eu.ehri.project.models.DocumentaryUnit;
-import eu.ehri.project.models.base.AccessibleEntity;
+import eu.ehri.project.models.Repository;
 import eu.ehri.project.models.base.Description;
 import eu.ehri.project.models.events.SystemEvent;
 import java.io.InputStream;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author linda
  */
-public class IcaAtomEadSingleEad extends AbstractImporterTest{
-    protected final String SINGLE_EAD = "single-ead.xml";
-
+public class IcaAtomMultiLeveledTest extends AbstractImporterTest{
+    private static final Logger logger = LoggerFactory.getLogger(IcaAtomMultiLeveledTest.class);
+     protected final String SINGLE_EAD = "zbirka-gradiva-za-povijest-zidova-collection-of-material-concerning-history-of-jews.xml";
+private final String UN_REL = "HR-HDA145corporateBodyAccessCroatianStateArchive";
     // Depends on fixtures
     protected final String TEST_REPO = "r1";
 
     // Depends on single-ead.xml
-    protected final String IMPORTED_ITEM_ID = "C00001";
+    protected final String IMPORTED_ITEM_ID = "HR r000382HR HR-HDA 1551";
 
     @Test
     public void testImportItemsT() throws Exception {
         Repository agent = manager.getFrame(TEST_REPO, Repository.class);
-        final String logMessage = "Importing a single EAD by IcaAtomEadSingleEad";
+        final String logMessage = "Importing a multileveled EAD by IcaAtomMultiLeveled";
 
         int origCount = getNodeCount(graph);
 
@@ -40,31 +44,36 @@ public class IcaAtomEadSingleEad extends AbstractImporterTest{
 
         printGraph(graph);
         // How many new nodes will have been created? We should have
-        // - 1 more DocumentaryUnit
-        // - 1 more DocumentDescription
-        // - 1 more DatePeriod
+        // - 3 more DocumentaryUnit
+        // - 3 more DocumentDescription
+        // - 3 more DatePeriod
         //TODO: test these UR's
         // - 4 more UndeterminedRelationships
         //TODO: test this UP
-        // - 1 more UnknownProperty
-        // - 2 more import Event links
+        // - 3 more UnknownProperty
+        // - 4 more import Event links
         // - 1 more import Event
 
-        int createCount = origCount + 11;
+        int createCount = origCount + 21;
 
         // - 4 more UnderterminedRelationship nodes
 
         assertEquals(createCount, getNodeCount(graph));
 
         // Yet we've only created 1 *logical* item...
-        assertEquals(1, log.getChanged());
+        assertEquals(3, log.getChanged());
 
         Iterable<Vertex> docs = graph.getVertices("identifier",
                 IMPORTED_ITEM_ID);
         assertTrue(docs.iterator().hasNext());
         DocumentaryUnit unit = graph.frame(docs.iterator().next(), DocumentaryUnit.class);
         for(Description d : unit.getDocumentDescriptions())
-            assertEquals("Test EAD Item", d.getName());
+            assertEquals("Zbirka gradiva za povijest Židova (Collection of material concerning the history of Jews)", d.getName());
+//  UndeterminedRelationship creator = graph.frame(graph.getVertices("identifier", UN_REL).iterator().next(), UndeterminedRelationship.class);
+//        for(String key : creator.asVertex().getPropertyKeys()){
+//            logger.debug(key + ": " + creator.asVertex().getProperty(key));
+//        }
+//        assertTrue(creator.asVertex().getPropertyKeys().contains("role"));
 
         List<SystemEvent> actions = toList(unit.getHistory());
         // Check we've only got one action
@@ -76,16 +85,13 @@ public class IcaAtomEadSingleEad extends AbstractImporterTest{
         ImportLog log2 = new SaxImportManager(graph, agent, validUser, IcaAtomEadImporter.class, IcaAtomEadHandler.class).importFile(ios2, logMessage);
 
         // We should only have three more nodes, for 
-        // the action and 
-        // the user event links, 
-        // plus the global event
-        assertEquals(createCount + 3, getNodeCount(graph));
+        // 1 the action and 
+        // 3 the user event links, 
+        // 1 plus the global event
+        assertEquals(createCount + 5, getNodeCount(graph));
         // And one logical item should've been updated
-        assertEquals(1, log2.getUpdated());
+        assertEquals(3, log2.getUpdated());
 
-        // Check permission scopes
-        for (AccessibleEntity e : log.getAction().getSubjects()) {
-            assertEquals(agent, e.getPermissionScope());
-        }
+        // TODO: Check permission scopes
     }
 }
