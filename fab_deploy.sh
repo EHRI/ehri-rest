fe56356e36a9d62e3660fc382954f5569d987479
bencomp <ben@companjen.name>
2014-01-27 18:03:25 +0100

    Update allowedProperties: add or update relatedMaterial; add files to copy and some functions to fabfile, update fab_deploy script, update import-ushmm

--- fab_deploy.sh
+++ fab_deploy.sh
@@ -21,6 +21,11 @@ fab stage clean_deploy
 # op staging (en productie?): /home/benc/import-data/<country code>/<repository name>
 # e.g. /home/benc/import-data/nl/niod
 
+# For a first time "user add", create a new user in the portal: "More" -> "Users" -> "Create User". Use the username that will be the identifier of the user profile that you re-add later.
+# Add your import command to queue.sh (queue.sh MUST NOT be in the git repository)
+# Add the required users to queue.sh (remember? the names that you added via the portal)
+# (We assume everyone who has access to the staging server is allowed to view everything that is on the staging server)
+
 # Zorg ervoor dat lib.sh up to date is.
 # It's not necessary to do this on every deployment.
 
@@ -40,4 +45,4 @@ fab stage update_db:/tmp/cloned_db
 
 
 # voer imports uit
-
+fab stage load_queue
