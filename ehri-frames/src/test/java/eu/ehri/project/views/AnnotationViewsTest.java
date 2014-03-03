635e83a31e45773f7b8210f43d2455c4409bd1ee
Mike Bryant <mikesname@gmail.com>
2014-03-03 14:30:22 +0000

    Add a new group specifically for making annotations

--- ehri-frames/src/test/java/eu/ehri/project/views/AnnotationViewsTest.java
+++ ehri-frames/src/test/java/eu/ehri/project/views/AnnotationViewsTest.java
@@ -2,14 +2,10 @@ package eu.ehri.project.views;
 
 import com.google.common.collect.Lists;
 import eu.ehri.project.acl.AclManager;
-import eu.ehri.project.acl.PermissionType;
 import eu.ehri.project.definitions.Ontology;
 import eu.ehri.project.exceptions.AccessDenied;
 import eu.ehri.project.exceptions.PermissionDenied;
-import eu.ehri.project.models.Annotation;
-import eu.ehri.project.models.ContentType;
-import eu.ehri.project.models.EntityClass;
-import eu.ehri.project.models.UserProfile;
+import eu.ehri.project.models.*;
 import eu.ehri.project.models.base.Accessor;
 import eu.ehri.project.persistence.Bundle;
 import eu.ehri.project.test.AbstractFixtureTest;
@@ -24,6 +20,7 @@ public class AnnotationViewsTest extends AbstractFixtureTest {
     public AclManager acl;
     public UserProfile admin;
     public UserProfile user;
+    public Group canAnnotate;
 
     @Before
     public void setUp() throws Exception {
@@ -32,6 +29,7 @@ public class AnnotationViewsTest extends AbstractFixtureTest {
         av = new AnnotationViews(graph);
         admin = manager.getFrame("mike", UserProfile.class);
         user = manager.getFrame("reto", UserProfile.class);
+        canAnnotate = manager.getFrame("portal", Group.class);
     }
 
     @Test(expected = PermissionDenied.class)
@@ -45,9 +43,7 @@ public class AnnotationViewsTest extends AbstractFixtureTest {
 
     @Test(expected = AccessDenied.class)
     public void testCreateForWithoutAccess() throws Exception {
-        acl.grantPermissions(user, manager.getFrame(
-                EntityClass.DOCUMENTARY_UNIT.getName(), ContentType.class),
-                PermissionType.ANNOTATE);
+        canAnnotate.addMember(user);
         Bundle ann = new Bundle(EntityClass.ANNOTATION)
                 .withDataValue(Ontology.ANNOTATION_NOTES_BODY, "test");
         Annotation annotation = av
@@ -57,9 +53,7 @@ public class AnnotationViewsTest extends AbstractFixtureTest {
 
     @Test(expected = PermissionDenied.class)
     public void testCreateForOutsideSubtree() throws Exception {
-        acl.grantPermissions(user, manager.getFrame(
-                EntityClass.DOCUMENTARY_UNIT.getName(), ContentType.class),
-                PermissionType.ANNOTATE);
+        canAnnotate.addMember(user);
         Bundle ann = new Bundle(EntityClass.ANNOTATION)
                 .withDataValue(Ontology.ANNOTATION_NOTES_BODY, "test");
         Annotation annotation = av
@@ -69,9 +63,7 @@ public class AnnotationViewsTest extends AbstractFixtureTest {
 
     @Test
     public void testCreateFor() throws Exception {
-        acl.grantPermissions(user, manager.getFrame(
-                EntityClass.DOCUMENTARY_UNIT.getName(), ContentType.class),
-                PermissionType.ANNOTATE);
+        canAnnotate.addMember(user);
         Bundle ann = new Bundle(EntityClass.ANNOTATION)
                 .withDataValue(Ontology.ANNOTATION_NOTES_BODY, "test");
         Annotation annotation = av
