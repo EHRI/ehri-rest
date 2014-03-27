d84d39231aa4e1020d13b99cf9056c5928501d28
Mike Bryant <mikesname@gmail.com>
2014-03-27 16:12:27 +0000

    Add a command for creating arbitrary edges between nodes

---
+++ ehri-cmdline/src/test/java/eu/ehri/project/commands/RelationAddTest.java
@@ -0,0 +1,83 @@
+package eu.ehri.project.commands;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Vertex;
+import eu.ehri.project.models.EntityClass;
+import eu.ehri.project.test.AbstractFixtureTest;
+import eu.ehri.project.test.GraphTestBase;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.PosixParser;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Mike Bryant (http://github.com/mikesname)
+ */
+public class RelationAddTest extends GraphTestBase {
+
+    private Vertex mike;
+    private Vertex linda;
+    private Vertex reto;
+    private RelationAdd relationAdd;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        relationAdd = new RelationAdd();
+        mike = manager.createVertex("mike", EntityClass.USER_PROFILE, Maps.<String,Object>newHashMap());
+        reto = manager.createVertex("reto", EntityClass.USER_PROFILE, Maps.<String,Object>newHashMap());
+        linda = manager.createVertex("linda", EntityClass.USER_PROFILE, Maps.<String,Object>newHashMap());
+    }
+
+    @Test
+    public void testAddRelation() throws Exception {
+        assertEquals(0L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        CommandLine commandLine = relationAdd.getCmdLine(new String[]{"mike", "knows", "linda"});
+        int retVal = relationAdd.execWithOptions(graph, commandLine);
+        assertEquals(0, retVal);
+        assertEquals(1L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        assertEquals(1L, Iterables.size(linda.getVertices(Direction.IN, "knows")));
+
+        relationAdd.execWithOptions(graph, commandLine);
+        assertEquals(2L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        assertEquals(2L, Iterables.size(linda.getVertices(Direction.IN, "knows")));
+    }
+
+    @Test
+    public void testAddUniqueRelation() throws Exception {
+        assertEquals(0L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        CommandLine commandLine = relationAdd.getCmdLine(new String[]{"-u", "mike", "knows", "linda"});
+        int retVal = relationAdd.execWithOptions(graph, commandLine);
+        assertEquals(0, retVal);
+        assertEquals(1L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        assertEquals(1L, Iterables.size(linda.getVertices(Direction.IN, "knows")));
+
+        relationAdd.execWithOptions(graph, commandLine);
+        assertEquals(1L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        assertEquals(1L, Iterables.size(linda.getVertices(Direction.IN, "knows")));
+    }
+
+    @Test
+    public void testAddSingleRelation() throws Exception {
+        assertEquals(0L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        CommandLine commandLine1 = relationAdd.getCmdLine(new String[]{"mike", "knows", "linda"});
+        int retVal = relationAdd.execWithOptions(graph, commandLine1);
+        assertEquals(0, retVal);
+        assertEquals(1L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        assertEquals(1L, Iterables.size(linda.getVertices(Direction.IN, "knows")));
+
+        CommandLine commandLine2 = relationAdd.getCmdLine(new String[]{"-s", "mike", "knows", "reto"});
+        relationAdd.execWithOptions(graph, commandLine2);
+        assertEquals(1L, Iterables.size(mike.getVertices(Direction.OUT, "knows")));
+        assertEquals(0L, Iterables.size(linda.getVertices(Direction.IN, "knows")));
+        assertEquals(1L, Iterables.size(reto.getVertices(Direction.IN, "knows")));
+    }
+}
