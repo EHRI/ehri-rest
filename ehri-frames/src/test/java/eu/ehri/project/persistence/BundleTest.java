53de960e5c013db92aba170364224e5213a89139
lindar <linda.reijnhoudt@dans.knaw.nl>
2014-04-04 15:01:25 +0200

    importing an ead file with a different language will not delete other existing descriptions

--- ehri-frames/src/test/java/eu/ehri/project/persistence/BundleTest.java
+++ ehri-frames/src/test/java/eu/ehri/project/persistence/BundleTest.java
@@ -163,10 +163,10 @@ public class BundleTest {
             }
         };
         Bundle filtered = bundle.filterRelations(filter);
-        assertFalse(bundle.getRelations(Ontology.DESCRIPTION_FOR_ENTITY).isEmpty());
+        assertFalse((Ontology.DESCRIPTION_FOR_ENTITY).isEmpty());
         assertTrue(filtered.getRelations(Ontology.DESCRIPTION_FOR_ENTITY).isEmpty());
     }
-
+        
     @Test
     public void testGetRelations() throws Exception {
         // TODO
