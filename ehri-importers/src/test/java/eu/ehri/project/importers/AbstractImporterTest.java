752354e13b2740bb4228a1b23a28360bf168e2c3
Mike Bryant <mikesname@gmail.com>
2014-04-22 16:03:25 +0100

    More debug utils

--- ehri-importers/src/test/java/eu/ehri/project/importers/AbstractImporterTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/AbstractImporterTest.java
@@ -1,10 +1,14 @@
 package eu.ehri.project.importers;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.frames.FramedGraph;
 import eu.ehri.project.definitions.Ontology;
+import eu.ehri.project.models.annotations.EntityType;
 import eu.ehri.project.models.base.Description;
 import eu.ehri.project.models.cvoc.Concept;
 import eu.ehri.project.test.AbstractFixtureTest;
@@ -13,6 +17,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.PrintStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 
 /**
@@ -20,6 +27,71 @@ import java.io.PrintStream;
  */
 public class AbstractImporterTest extends AbstractFixtureTest {
     private static final Logger logger = LoggerFactory.getLogger(AbstractImporterTest.class);
+
+    /**
+     * Util class to hold node data (for debugging the graph)
+     */
+    public static class Proxy {
+        final String id;
+        final String type;
+        final Object gid;
+        final Map<String,Object> data;
+        public Proxy(Vertex v) {
+            this.id = v.getProperty(EntityType.ID_KEY);
+            this.type = v.getProperty(EntityType.TYPE_KEY);
+            this.gid = v.getId();
+            data = Maps.newHashMap();
+            for (String k : v.getPropertyKeys()) {
+                if (!(k.equals(EntityType.ID_KEY) || k.equals(EntityType.TYPE_KEY))) {
+                    data.put(k, v.getProperty(k));
+                }
+            }
+        }
+        public String toString() {
+            return "<" + id + " (" + type + ") [" + gid + "]>";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Proxy proxy = (Proxy) o;
+
+            return gid.equals(proxy.gid)
+                    && !(id != null ? !id.equals(proxy.id) : proxy.id != null)
+                    && !(type != null ? !type.equals(proxy.type) : proxy.type != null);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = id != null ? id.hashCode() : 0;
+            result = 31 * result + (type != null ? type.hashCode() : 0);
+            result = 31 * result + gid.hashCode();
+            return result;
+        }
+    }
+
+    public List<Proxy> getGraphState(FramedGraph<?> graph) {
+        List<Proxy> list = Lists.newArrayList();
+        for (Vertex v : graph.getVertices()) {
+            list.add(new Proxy(v));
+        }
+        return list;
+    }
+
+    public void diffGraph(List<Proxy> list1, List<Proxy> list2) {
+        Set<Proxy> added = Sets.newHashSet(list2);
+        added.removeAll(list1);
+        for (Proxy proxy : added) {
+            logger.debug(" - Added:   " + proxy);
+        }
+        Set<Proxy> removed = Sets.newHashSet(list1);
+        removed.removeAll(list2);
+        for (Proxy proxy : removed) {
+            logger.debug(" - Removed: " + proxy);
+        }
+    }
 
     protected void printGraph(FramedGraph<?> graph) {
         int vcount = 0;
@@ -30,7 +102,7 @@ public class AbstractImporterTest extends AbstractFixtureTest {
                 if (v.getProperty(key) instanceof String[]) {
                     String[] list = (String[]) v.getProperty(key);
                     for (String o : list) {
-                        value += "[" + o.toString() + "] ";
+                        value += "[" + o + "] ";
                     }
                 } else {
                     value = v.getProperty(key).toString();
