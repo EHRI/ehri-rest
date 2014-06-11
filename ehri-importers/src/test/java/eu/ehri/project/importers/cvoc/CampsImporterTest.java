8401221d4d2813a9eec0cb00c7c880e06df11357
lindar <linda.reijnhoudt@dans.knaw.nl>
2014-06-11 08:58:47 +0200

    included editorialNote in skosImporter

--- ehri-importers/src/test/java/eu/ehri/project/importers/cvoc/CampsImporterTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/cvoc/CampsImporterTest.java
@@ -23,7 +23,8 @@ import org.slf4j.LoggerFactory;
  */
 public class CampsImporterTest extends AbstractImporterTest {
     private static final Logger logger = LoggerFactory.getLogger(CampsImporterTest.class);
-    protected final String SKOS_FILE = "cvoc/camps.rdf";
+    protected final String SKOS_FILE = "camps.rdf";
+    protected final String SKOS_FILE_VERSION2 = "campsv02.rdf";
 
     @Test
     public void testImportItemsT() throws Exception {
@@ -43,7 +44,7 @@ public class CampsImporterTest extends AbstractImporterTest {
         /*  How many new nodes will have been created? We should have
          * 8 more Concepts
        	 * 8 more ConceptDescription
-	     * 9 more import Event links (8 for every Unit, 1 for the User)
+	 * 9 more import Event links (8 for every Unit, 1 for the User)
          * 1 more import Event
          */
         int afterNodeCount = count + 26;
@@ -64,4 +65,61 @@ public class CampsImporterTest extends AbstractImporterTest {
             assertEquals(vocabulary, e.getPermissionScope());
         }
     }
+
+    @Test
+    public void testImportItemsVersion2() throws Exception {
+
+        Vocabulary vocabulary = manager.getFrame("cvoc1", Vocabulary.class);
+
+        int count = getNodeCount(graph);
+        int voccount = toList(vocabulary.getConcepts()).size();
+        InputStream ios = ClassLoader.getSystemResourceAsStream(SKOS_FILE);
+        SkosCoreCvocImporter importer = new SkosCoreCvocImporter(graph, validUser, vocabulary);
+        importer.setTolerant(true);
+        ImportLog log = importer.importFile(ios, "Importing the camps as a SKOS file");
+
+        printGraph(graph);
+        /*  How many new nodes will have been created? We should have
+         * 8 more Concepts
+       	 * 8 more ConceptDescription
+	 * 9 more import Event links (8 for every Unit, 1 for the User)
+         * 1 more import Event
+         */
+        int afterNodeCount = count + 26;
+        assertEquals(afterNodeCount, getNodeCount(graph));
+        assertEquals(voccount + 8, toList(vocabulary.getConcepts()).size());
+
+        // get a top concept
+        String skosConceptId = "675";
+        Query<Concept> query = new Query<Concept>(graph, Concept.class);
+        // Query for document identifier.
+        List<Concept> list = toList(query.setLimit(1).list(
+                Ontology.IDENTIFIER_KEY, skosConceptId, validUser));
+
+        assertEquals(1, toList(list.get(0).getBroaderConcepts()).size());
+
+        // Check permission scopes
+        for (AccessibleEntity e : log.getAction().getSubjects()) {
+            assertEquals(vocabulary, e.getPermissionScope());
+        }
+        
+        //import version 2
+        ios = ClassLoader.getSystemResourceAsStream(SKOS_FILE_VERSION2);
+        importer = new SkosCoreCvocImporter(graph, validUser, vocabulary);
+        importer.setTolerant(true);
+        log = importer.importFile(ios, "Importing the modified camps as a SKOS file");
+
+         printGraph(graph);
+        /*  How many new nodes will have been created? We should have
+         * 0 new concepts, 2 modified concepts (counts as 0)
+       	 * 0 new ConceptDescription, 2 modified (counts as 0)
+         * 2 more import Event links (2 for every modified Unit, 1 for the User)
+         * 1 more import Event
+         */
+         afterNodeCount = count + 26 + 3;
+        assertEquals(afterNodeCount, getNodeCount(graph));
+        assertEquals(voccount + 8, toList(vocabulary.getConcepts()).size());
+
+    }
+
 }
--- ehri-importers/src/test/java/eu/ehri/project/importers/cvoc/SkosImporterAdminDistrictTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/cvoc/SkosImporterAdminDistrictTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 
 import eu.ehri.project.importers.ImportLog;
 import eu.ehri.project.importers.AbstractImporterTest;
+import eu.ehri.project.importers.SaxImportManager;
 import eu.ehri.project.models.Repository;
 import eu.ehri.project.models.UserProfile;
 import eu.ehri.project.models.cvoc.Vocabulary;
@@ -34,12 +35,13 @@ public class SkosImporterAdminDistrictTest extends AbstractImporterTest {
         UserProfile user = validUser; //graph.frame(graph.getVertex(validUserId), UserProfile.class);
         Repository agent = manager.getFrame(TEST_REPO, Repository.class); //graph.frame(helper.getTestVertex(TEST_REPO), Repository.class);
 
-        final String logMessage = "Importing a single skos";
+        final String logMessage = "Importing a single skos: " + SINGLE_SKOS;
 
         int count = getNodeCount(graph);
         Vocabulary vocabulary = manager.getFrame("cvoc1", Vocabulary.class);
         InputStream ios = ClassLoader.getSystemResourceAsStream(SINGLE_SKOS);
-        SkosCoreCvocImporter importer = new SkosCoreCvocImporter(graph, validUser, vocabulary);
+//        SkosCoreCvocImporter importer = new SkosCoreCvocImporter(graph, validUser, vocabulary);
+        SkosImporter importer = SkosImporterFactory.newSkosImporter(graph, validUser, vocabulary);
         importer.setTolerant(true);
         ImportLog log = importer.importFile(ios, logMessage);
 
@@ -49,7 +51,7 @@ public class SkosImporterAdminDistrictTest extends AbstractImporterTest {
         // - 3 more Concept
         // - 4 more ConceptDescription ( 3 de + 1 fr )
         // - 4 more ImportEvents ( 3 + 1 )
-        // - 1 more import Action        
+        // - 1 more import Action
         assertEquals(count + 12, getNodeCount(graph));
 
     }
