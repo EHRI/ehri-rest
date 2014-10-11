aec56f6a713bd9f062574824a78a65440b9e5ddb
bencomp <ben@companjen.name>
2014-10-11 15:34:21 +0200

    Correct param name in javadoc

--- ehri-importers/src/main/java/eu/ehri/project/importers/EadHandler.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/EadHandler.java
@@ -280,7 +280,7 @@ public class EadHandler extends SaxXmlHandler {
      * for the description if no language is found.
      *
      * @param currentGraph    Data at the current node level
-     * @param eadLanguage Language code to use as default
+     * @param defaultLanguage Language code to use as default
      */
     protected void useDefaultLanguage(Map<String, Object> currentGraph, String defaultLanguage) {
 
