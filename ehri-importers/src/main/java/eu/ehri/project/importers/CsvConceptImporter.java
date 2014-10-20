277757f8d62f480a2ccecd6962d040a8dfe2da3e
lindar <linda.reijnhoudt@dans.knaw.nl>
2014-10-20 10:27:17 +0200

    Import FAST-subjects from a CSV file and link the Personalities

---
+++ ehri-importers/src/main/java/eu/ehri/project/importers/CsvConceptImporter.java
@@ -0,0 +1,158 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.ehri.project.importers;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.tinkerpop.frames.FramedGraph;
+import eu.ehri.project.acl.SystemScope;
+import eu.ehri.project.definitions.Ontology;
+import eu.ehri.project.exceptions.ValidationError;
+import eu.ehri.project.models.EntityClass;
+import eu.ehri.project.models.base.AccessibleEntity;
+import eu.ehri.project.models.base.Description;
+import eu.ehri.project.models.base.PermissionScope;
+import eu.ehri.project.models.cvoc.AuthoritativeItem;
+import eu.ehri.project.models.cvoc.AuthoritativeSet;
+import eu.ehri.project.persistence.Bundle;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import eu.ehri.project.persistence.BundleDAO;
+import eu.ehri.project.persistence.Mutation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author linda
+ */
+public class CsvConceptImporter extends XmlImporter<Object> {
+
+//    private final XmlImportProperties p = new XmlImportProperties("csvconcept.properties");
+
+    private static final Logger logger = LoggerFactory.getLogger(CsvConceptImporter.class);
+
+    public CsvConceptImporter(FramedGraph<?> framedGraph, PermissionScope permissionScope, ImportLog log) {
+        super(framedGraph, permissionScope, log);
+    }
+
+    @Override
+    public AccessibleEntity importItem(Map<String, Object> itemData) throws ValidationError {
+
+        BundleDAO persister = getPersister();
+
+        Bundle unit = new Bundle(EntityClass.CVOC_CONCEPT, extractUnit(itemData));
+
+        Bundle descBundle = new Bundle(EntityClass.CVOC_CONCEPT_DESCRIPTION, extractUnitDescription(itemData, EntityClass.CVOC_CONCEPT_DESCRIPTION));
+
+        unit = unit.withRelation(Ontology.DESCRIPTION_FOR_ENTITY, descBundle);
+
+        Mutation<AuthoritativeItem> mutation = persister.createOrUpdate(unit, AuthoritativeItem.class);
+        AuthoritativeItem frame = mutation.getNode();
+
+        if (!permissionScope.equals(SystemScope.getInstance())
+                && mutation.created()) {
+            manager.cast(permissionScope, AuthoritativeSet.class).addItem(frame);
+            frame.setPermissionScope(permissionScope);
+        }
+
+        handleCallbacks(mutation);
+        return frame;
+
+    }
+
+    @Override
+    public AccessibleEntity importItem(Map<String, Object> itemData, List<String> idPath) throws
+            ValidationError {
+        throw new UnsupportedOperationException("Not supported ever.");
+    }
+
+    private Map<String, Object> extractUnit(Map<String, Object> itemData) {
+        //unit needs at least IDENTIFIER_KEY
+        Map<String, Object> item = new HashMap<String, Object>();
+        if (itemData.containsKey("id")) {
+            item.put(Ontology.IDENTIFIER_KEY, itemData.get("id"));
+        } else {
+            logger.error("missing objectIdentifier");
+        }
+        return item;
+    }
+
+    private String getName(Map<String, Object> itemData) {
+        // FIXME: This all sucks
+        String firstName = (String) itemData.get("Firstname");
+        String lastName = (String) itemData.get("Lastname");
+        if (firstName == null && lastName == null) {
+            return null;
+        }
+        String name = "";
+        if (lastName != null) {
+            name = lastName;
+        }
+        if (firstName != null) {
+            name = firstName + " " + name;
+        }
+        return name;
+    }
+
+    private Map<String, Object> extractUnitDescription(Map<String, Object> itemData, EntityClass entityClass) {
+        Map<String, Object> item = new HashMap<String, Object>();
+        item.put(Ontology.CREATION_PROCESS, Description.CreationProcess.IMPORT.toString());
+
+
+        SaxXmlHandler.putPropertyInGraph(item, Ontology.NAME_KEY, itemData.get("name").toString());
+        for (String key : itemData.keySet()) {
+            if (!key.equals("id") && !key.equals("name")) {
+                    SaxXmlHandler.putPropertyInGraph(item, key, itemData.get(key).toString());
+            }
+
+        }
+        if (!item.containsKey("typeOfEntity")) {
+            SaxXmlHandler.putPropertyInGraph(item, "typeOfEntity", "subject");
+        }
+        if (!item.containsKey(Ontology.LANGUAGE_OF_DESCRIPTION)) {
+            SaxXmlHandler.putPropertyInGraph(item, Ontology.LANGUAGE_OF_DESCRIPTION, "en");
+        }
+        return item;
+    }
+
+
+    /**
+     * @param itemData
+     * @return returns a List with Maps with DatePeriod.DATE_PERIOD_START_DATE and DatePeriod.DATE_PERIOD_END_DATE values
+     */
+    @Override
+    public List<Map<String, Object>> extractDates(Map<String, Object> itemData) {
+
+        List<Map<String, Object>> l = Lists.newArrayList();
+        Map<String, Object> items = Maps.newHashMap();
+
+        String end = (String) itemData.get("DateofdeathYYYY-MM-DD");
+        String start = (String) itemData.get("DateofbirthYYYY-MM-DD");
+
+        if (start != null && start.endsWith("00-00")) {
+            start = start.substring(0, 4);
+        }
+        if (end != null && end.endsWith("00-00")) {
+            end = end.substring(0, 4);
+        }
+        if (end != null || start != null) {
+            if (start != null)
+                items.put(Ontology.DATE_PERIOD_START_DATE, start);
+            if (end != null)
+                items.put(Ontology.DATE_PERIOD_END_DATE, end);
+            l.add(items);
+        }
+        return l;
+    }
+
+    @Override
+    public Iterable<Map<String, Object>> extractDates(Object data) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
--- ehri-importers/src/test/java/eu/ehri/project/importers/PersonalitiesV2Test.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/PersonalitiesV2Test.java
@@ -4,13 +4,26 @@ import com.tinkerpop.blueprints.Vertex;
 import eu.ehri.project.acl.SystemScope;
 import eu.ehri.project.definitions.Entities;
 import eu.ehri.project.definitions.Ontology;
+import eu.ehri.project.exceptions.IntegrityError;
+import eu.ehri.project.exceptions.ItemNotFound;
+import eu.ehri.project.exceptions.PermissionDenied;
+import eu.ehri.project.exceptions.ValidationError;
+import eu.ehri.project.importers.exceptions.InputParseError;
 import eu.ehri.project.importers.properties.XmlImportProperties;
+import eu.ehri.project.models.EntityClass;
 import eu.ehri.project.models.HistoricalAgent;
 import eu.ehri.project.models.HistoricalAgentDescription;
 import eu.ehri.project.models.Link;
+import eu.ehri.project.models.Repository;
+import eu.ehri.project.models.UndeterminedRelationship;
 import eu.ehri.project.models.UnknownProperty;
 import eu.ehri.project.models.base.*;
+import eu.ehri.project.models.cvoc.Concept;
+import eu.ehri.project.models.cvoc.Vocabulary;
 import eu.ehri.project.models.events.SystemEvent;
+import eu.ehri.project.persistence.Bundle;
+import eu.ehri.project.views.impl.CrudViews;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import static org.junit.Assert.*;
@@ -28,7 +41,7 @@ public class PersonalitiesV2Test extends AbstractImporterTest {
     private static final Logger logger = LoggerFactory.getLogger(PersonalitiesV2Test.class);
 
     @Test
-    public void testAbwehrWithAllReferredNodes() throws Exception {
+    public void newPersonalitiesWithoutReferredNodes() throws Exception {
         final String SINGLE_EAC = "PersonalitiesV2.xml";
         final String logMessage = "Importing EAC " + SINGLE_EAC;
         InputStream ios = ClassLoader.getSystemResourceAsStream(SINGLE_EAC);
@@ -60,6 +73,77 @@ public class PersonalitiesV2Test extends AbstractImporterTest {
             assertEquals(2, ((List)d.asVertex().getProperty("otherFormsOfName")).size());
             assertTrue(d.asVertex().getProperty("place") instanceof List);
             assertEquals(2, ((List)d.asVertex().getProperty("place")).size());
+        }
+    }
+    
+    @Test
+    public void newPersonalitiesWithReferredNodes() throws ItemNotFound, PermissionDenied, ValidationError, IntegrityError, IOException, InputParseError{
+        final String TEST_REPO = "r1";
+
+        Repository agent = manager.getFrame(TEST_REPO, Repository.class);
+        Bundle vocabularyBundle = new Bundle(EntityClass.CVOC_VOCABULARY)
+                                .withDataValue(Ontology.IDENTIFIER_KEY, "FAST_keywords")
+                                .withDataValue(Ontology.NAME_KEY, "FAST Keywords");
+        Bundle conceptBundle = new Bundle(EntityClass.CVOC_CONCEPT)
+                                .withDataValue(Ontology.IDENTIFIER_KEY, "fst894382");
+        Vocabulary vocabulary = new CrudViews<Vocabulary>(graph, Vocabulary.class).create(vocabularyBundle, validUser);
+        logger.debug(vocabulary.getId());
+        Concept concept_716 = new CrudViews<Concept>(graph, Concept.class).create(conceptBundle, validUser); 
+        vocabulary.addItem(concept_716);
+        
+        
+        Vocabulary vocabularyTest = manager.getFrame("fast-keywords", Vocabulary.class);
+        assertNotNull(vocabularyTest);
+
+        
+        final String SINGLE_EAC = "PersonalitiesV2.xml";
+        final String logMessage = "Importing EAC " + SINGLE_EAC;
+        InputStream ios = ClassLoader.getSystemResourceAsStream(SINGLE_EAC);
+        int count = getNodeCount(graph);
+
+        // Before...
+        List<VertexProxy> graphState1 = getGraphState(graph);
+        
+        ImportLog log = new SaxImportManager(graph, SystemScope.getInstance(), validUser, EacImporter.class,
+                EacHandler.class, new XmlImportProperties("personalitiesv2.properties")).setTolerant(Boolean.TRUE).importFile(ios, logMessage);
+       // After...
+       List<VertexProxy> graphState2 = getGraphState(graph);
+       GraphDiff diff = diffGraph(graphState1, graphState2);
+       diff.printDebug(System.out);
+       /**
+        * null: 2
+        * relationship: 1
+        * historicalAgent: 1
+        * link: 1
+        * maintenanceEvent: 1
+        * systemEvent: 1
+        * historicalAgentDescription: 1
+        */
+        assertEquals(count+8, getNodeCount(graph));
+//        printGraph(graph);
+        HistoricalAgent person = manager.getFrame("ehri-pers-000051", HistoricalAgent.class);
+        for (Description d : person.getDescriptions()) {
+            for (UndeterminedRelationship rel : d.getUndeterminedRelationships()) {
+                if (rel.getRelationshipType().equals("subjectAccess")) {
+                    if (rel.getName().equals("Diplomatic documents")) {
+                        assertEquals(1, toList(rel.getLinks()).size());
+                        for (Link link : rel.getLinks()) {
+                            boolean conceptFound = false;
+                            for (LinkableEntity le : link.getLinkTargets()) {
+                                if (le.getType().equals("cvocConcept")) {
+                                    assertEquals(le, concept_716);
+                                    conceptFound = true;
+                                }
+                            }
+                            assertTrue(conceptFound);
+                            logger.debug(link.getLinkType());
+                            for (String key : link.asVertex().getPropertyKeys()) {
+                                logger.debug(key + ":" + link.asVertex().getProperty(key));
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
