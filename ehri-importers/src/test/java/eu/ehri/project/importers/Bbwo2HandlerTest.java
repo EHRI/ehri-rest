21157bc26b7e3f92f0e5a818f6798f81d155673b
lindar <linda.reijnhoudt@dans.knaw.nl>
2014-12-11 09:53:55 +0100

    Link subjectAccess with the right attributes to the appropriate Vocabulary.
    this was done for the bbwo2 beeldbank collection description. subject-tags were modified from
    <dc:subject xmlns="http://www.openarchives.org/OAI/2.0/">Kinderen</dc:subject>
    to
    <dc:subject xmlns="http://www.openarchives.org/OAI/2.0/" id="kinderen" source="niod-trefwoorden" type="subjectAccess" term="Kinderen">Kinderen</dc:subject>
    
    so it will create a Link to the Concept 'kinderen' in de Vocabulary 'niod-trefwoorden'

--- ehri-importers/src/test/java/eu/ehri/project/importers/Bbwo2HandlerTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/Bbwo2HandlerTest.java
@@ -1,23 +1,37 @@
 package eu.ehri.project.importers;
 
+import eu.ehri.project.definitions.Ontology;
+import eu.ehri.project.exceptions.IntegrityError;
 import eu.ehri.project.exceptions.ItemNotFound;
+import eu.ehri.project.exceptions.PermissionDenied;
 import eu.ehri.project.exceptions.ValidationError;
 import eu.ehri.project.importers.exceptions.InputParseError;
 import eu.ehri.project.importers.properties.XmlImportProperties;
 import eu.ehri.project.models.DocumentDescription;
 import eu.ehri.project.models.DocumentaryUnit;
+import eu.ehri.project.models.EntityClass;
+import eu.ehri.project.models.Link;
+import eu.ehri.project.models.UndeterminedRelationship;
+import eu.ehri.project.models.base.Description;
+import eu.ehri.project.models.base.LinkableEntity;
 import eu.ehri.project.models.base.PermissionScope;
+import eu.ehri.project.models.cvoc.Concept;
+import eu.ehri.project.models.cvoc.Vocabulary;
+import eu.ehri.project.persistence.Bundle;
+import eu.ehri.project.views.impl.CrudViews;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Linda Reijnhoudt (https://github.com/lindareijnhoudt)
  */
 public class Bbwo2HandlerTest extends AbstractImporterTest {
-
+    private static final Logger logger = LoggerFactory.getLogger(Bbwo2HandlerTest.class);
     protected final String TEST_REPO = "r1";
     protected final String XMLFILE_NL = "bbwo2.xml";
     protected final String ARCHDESC = "1505";
@@ -25,13 +39,30 @@ public class Bbwo2HandlerTest extends AbstractImporterTest {
     int origCount = 0;
 
     @Test
-    public void bbwo2Test() throws ItemNotFound, IOException, ValidationError, InputParseError {
+    public void bbwo2Test() throws ItemNotFound, IOException, ValidationError, InputParseError, PermissionDenied, IntegrityError {
 
         PermissionScope agent = manager.getFrame(TEST_REPO, PermissionScope.class);
         final String logMessage = "Importing an example BBWO2 DC";
+        
+        //id="joodse-raad" source="niod-trefwoorden" term="Kinderen"
+        Bundle vocabularyBundle = new Bundle(EntityClass.CVOC_VOCABULARY)
+                                .withDataValue(Ontology.IDENTIFIER_KEY, "niod-trefwoorden")
+                                .withDataValue(Ontology.NAME_KEY, "NIOD Keywords");
+        Bundle conceptBundle = new Bundle(EntityClass.CVOC_CONCEPT)
+                                .withDataValue(Ontology.IDENTIFIER_KEY, "joodse-raad");
+        Vocabulary vocabulary = new CrudViews<Vocabulary>(graph, Vocabulary.class).create(vocabularyBundle, validUser);
+        Concept concept_716 = new CrudViews<Concept>(graph, Concept.class).create(conceptBundle, validUser); 
+        vocabulary.addItem(concept_716);
+        
+        
+        Vocabulary vocabularyTest = manager.getFrame("niod-trefwoorden", Vocabulary.class);
+        assertNotNull(vocabularyTest);
+
+        
         // Before...
         List<VertexProxy> graphState1 = getGraphState(graph);
-
+        
+        
         origCount = getNodeCount(graph);
         InputStream ios = ClassLoader.getSystemResourceAsStream(XMLFILE_NL);
         ImportLog log = new SaxImportManager(graph, agent, validUser, EadImporter.class, DcEuropeanaHandler.class, new XmlImportProperties("dceuropeana.properties")).importFile(ios, logMessage);
@@ -44,21 +75,54 @@ public class Bbwo2HandlerTest extends AbstractImporterTest {
         /**
          * How many new nodes will have been created? We should have 
          * 1 more DocumentaryUnits (record) 
+         * 1 more Link
          * 1 more DocumentDescription 
          * 1 more DatePeriod 
          * 4 relations (3 more subject + 1 more Place )
          * 2 more import Event links (1 for each Unit, 1 for the User) 
          * 1 more import Event 
          */
-        //temporary without subjects, still working on the DcEuropeanaHandler to link with existing cvocs
-        int newCount = origCount + 10 - 3;
+        int newCount = origCount + 11;
         assertEquals(newCount, getNodeCount(graph));
 
         DocumentaryUnit archdesc = graph.frame(getVertexByIdentifier(graph, ARCHDESC), DocumentaryUnit.class);
+        assertNotNull(archdesc);
         for (DocumentDescription d : archdesc.getDocumentDescriptions()) {
             assertEquals("More refugee children arrive from Germany - in time ...", d.getName());
             assertEquals("1505", d.asVertex().getProperty("sourceFileId"));
-            System.out.println(d.getId() + " - " + archdesc.asVertex().getProperty("identifier"));
+            logger.debug("id:"+d.getId() + " - identifier:" + archdesc.asVertex().getProperty("identifier"));
         }
+
+        for(Concept concept : vocabularyTest.getConcepts()){
+            logger.debug("concept:"+concept.getIdentifier());
+        }
+        
+        boolean passTest = false;
+        DocumentaryUnit person = manager.getFrame("nl-r1-1505", DocumentaryUnit.class);
+        for (Description d : person.getDescriptions()) {
+            for (UndeterminedRelationship rel : d.getUndeterminedRelationships()) {
+                if (rel.getRelationshipType().equals("subjectAccess")) {
+                    if (rel.getName().equals("kinderen")) {
+                        assertEquals(1, toList(rel.getLinks()).size());
+                        for (Link link : rel.getLinks()) {
+                            boolean conceptFound = false;
+                            for (LinkableEntity le : link.getLinkTargets()) {
+                                if (le.getType().equals("cvocConcept")) {
+                                    assertEquals(le, concept_716);
+                                    conceptFound = true;
+                                }
+                            }
+                            assertTrue(conceptFound);
+                            passTest=true;
+                            logger.debug(link.getLinkType());
+                            for (String key : link.asVertex().getPropertyKeys()) {
+                                logger.debug(key + ":" + link.asVertex().getProperty(key));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        assertTrue(passTest);
     }
 }
