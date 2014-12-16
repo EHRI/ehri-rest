9f0c371d2bd1836a95fbe399517f0d77ec088c8f
bencomp <ben@companjen.name>
2014-12-16 18:27:30 +0100

    Clean up file-listing files in import-large-batch.sh

--- scripts/import-large-batch.sh
+++ scripts/import-large-batch.sh
@@ -10,6 +10,9 @@ LOGFILE=$3
 PROPERTIES=$4
 HANDLER=$5
 
+# Since we append instead of write, make sure there is no such list of files yet.
+[ -e /opt/webapps/data/import-metadata/$SCOPE.txt ] && rm /opt/webapps/data/import-metadata/$SCOPE.txt
+
 for FILE in `ls /opt/webapps/data/import-data/$DATADIR`; do
     echo "/opt/webapps/data/import-data/$DATADIR/$FILE" >> /opt/webapps/data/import-metadata/$SCOPE.txt
 done
@@ -20,3 +23,6 @@ for TFILE in `ls /opt/webapps/data/import-metadata/$SCOPE-split*`; do
     head $TFILE
     curl -X POST -m 7200 -H "Authorization: $USER" --data-binary @$TFILE -H "Content-Type: text/plain" "http://localhost:7474/ehri/import/ead?scope=$SCOPE&log=$LOGFILE&&properties=$PROPERTIES&handler=$HANDLER"    
 done
+
+# Remove split files
+rm /opt/webapps/data/import-metadata/$SCOPE-split-*
