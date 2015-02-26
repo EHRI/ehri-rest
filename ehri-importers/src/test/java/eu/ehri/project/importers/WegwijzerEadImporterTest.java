40cccd5b15c9bf1b3d5d7e077982f94ebdec07b3
lindar <linda.reijnhoudt@dans.knaw.nl>
2015-02-26 08:16:59 +0100

    Test the import for the harvested Wegwijzer descriptions.

---
+++ ehri-importers/src/test/java/eu/ehri/project/importers/WegwijzerEadImporterTest.java
@@ -0,0 +1,60 @@
+package eu.ehri.project.importers;
+
+import eu.ehri.project.importers.properties.XmlImportProperties;
+import eu.ehri.project.models.DocumentDescription;
+import eu.ehri.project.models.DocumentaryUnit;
+import eu.ehri.project.models.Repository;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Linda Reijnhoudt (https://github.com/lindareijnhoudt)
+ */
+public class WegwijzerEadImporterTest extends AbstractImporterTest{
+    
+       protected final String SINGLE_EAD = "wegwijzer.xml";
+
+       // Depends on fixtures
+    protected final String TEST_REPO ="r1";
+
+    @Test
+    public void testImportItemsT() throws Exception {
+
+        final String logMessage = "Importing a single EAD";
+
+        int origCount = getNodeCount(graph);
+        System.out.println(origCount);
+        InputStream ios = ClassLoader.getSystemResourceAsStream(SINGLE_EAD);
+        importManager = new SaxImportManager(graph, repository, validUser, EadImporter.class, EadHandler.class, new XmlImportProperties("wegwijzer.properties"))
+                .setTolerant(Boolean.TRUE);
+        
+                 // Before...
+       List<VertexProxy> graphState1 = getGraphState(graph);
+        ImportLog log = importManager.importFile(ios, logMessage);
+        printGraph(graph);
+        
+ // After...
+       List<VertexProxy> graphState2 = getGraphState(graph);
+       GraphDiff diff = diffGraph(graphState1, graphState2);
+       diff.printDebug(System.out);
+       /**
+        * null: 7
+        * relationship: 27
+        * documentaryUnit: 6
+        * documentDescription: 6
+        * maintenanceEvent: 1
+        * systemEvent: 1
+        * datePeriod: 6
+        */
+        assertEquals(getNodeCount(graph), origCount+54);
+//        printGraph(graph);
+       
+
+
+        
+    }
+
+}
