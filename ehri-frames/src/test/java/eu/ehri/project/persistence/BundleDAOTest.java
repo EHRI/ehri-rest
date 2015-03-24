87b437cf0abb64265401a1015d98486ad104a85a
Mike Bryant <mikesname@gmail.com>
2015-03-24 11:04:03 +0000

    Apply EUPL copyright and license notice to all production and test Java files

--- ehri-frames/src/test/java/eu/ehri/project/persistence/BundleDAOTest.java
+++ ehri-frames/src/test/java/eu/ehri/project/persistence/BundleDAOTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2015 Data Archiving and Networked Services (an institute of
+ * Koninklijke Nederlandse Akademie van Wetenschappen), King's College London,
+ * Georg-August-Universitaet Goettingen Stiftung Oeffentlichen Rechts
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
 package eu.ehri.project.persistence;
 
 import com.google.common.collect.Iterables;
