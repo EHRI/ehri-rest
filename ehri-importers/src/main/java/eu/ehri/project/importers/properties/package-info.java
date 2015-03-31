aca516bcc145f06355624eb1d604c39c8ae24d2e
bencomp <ben@companjen.name>
2015-03-31 14:27:01 +0200

    Add package info to importer.cvoc and importers.properties packages

---
+++ ehri-importers/src/main/java/eu/ehri/project/importers/properties/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2015 Data Archiving and Networked Services (an institute of
+ * Koninklijke Nederlandse Akademie van Wetenschappen), King's College London,
+ * Georg-August-Universitaet Goettingen Stiftung Oeffentlichen Rechts
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
+/**
+ * Classes in this package support using .properties files to specify mappings
+ * between pseudo-XPath chunks and properties of nodes in the graph.
+ * @author Linda Reijnhoudt (https://github.com/lindareijnhoudt)
+ */
+package eu.ehri.project.importers.properties;
