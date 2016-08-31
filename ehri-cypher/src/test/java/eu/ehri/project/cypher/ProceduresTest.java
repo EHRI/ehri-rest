00873e12566998cc9338f6af03364e4ba4534e10
Mike Bryant <mikesname@gmail.com>
2016-08-31 12:01:40 +0100

    Add a Maven module to expose useful functions as Cypher procedures.

---
+++ ehri-cypher/src/test/java/eu/ehri/project/cypher/ProceduresTest.java
@@ -0,0 +1,45 @@
+package eu.ehri.project.cypher;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Values;
+import org.neo4j.harness.junit.Neo4jRule;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+public class ProceduresTest {
+
+    @Rule
+    public Neo4jRule neo4j = new Neo4jRule().withProcedure(Procedures.class);
+
+    @Test
+    public void testCountryCodeToName() throws Exception {
+        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.build().withEncryptionLevel(Config
+                .EncryptionLevel.NONE).toConfig())) {
+            StatementResult result = driver.session()
+                    .run("CALL eu.ehri.project.cypher.countryCodeToName({code})", Values.parameters("code", "us"));
+            assertThat(result.single().get("value").asString(), equalTo("United States"));
+        }
+    }
+
+    @Test
+    public void testLanguageCodeToName() throws Exception {
+        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.build().withEncryptionLevel(Config
+                .EncryptionLevel.NONE).toConfig())) {
+            StatementResult result1 = driver.session()
+                    .run("CALL eu.ehri.project.cypher.languageCodeToName({code})", Values.parameters("code", "en"));
+            assertThat(result1.single().get("value").asString(), equalTo("English"));
+
+            StatementResult result2 = driver.session()
+                    .run("CALL eu.ehri.project.cypher.languageCodeToName({code})", Values.parameters("code", "fra"));
+            assertThat(result2.single().get("value").asString(), equalTo("French"));
+        }
+    }
+}
