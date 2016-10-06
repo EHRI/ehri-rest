73b64706cb211ab7262d52b19ec3d45d1483f46c
Mike Bryant <mikesname@gmail.com>
2016-10-06 11:46:35 +0100

    Move some SKOS property definitions from the ontology to an enum

--- ehri-io/src/test/java/eu/ehri/project/exporters/cvoc/JenaSkosExporterTest.java
+++ ehri-io/src/test/java/eu/ehri/project/exporters/cvoc/JenaSkosExporterTest.java
@@ -32,7 +32,8 @@ import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class JenaSkosExporterTest extends AbstractSkosTest {
