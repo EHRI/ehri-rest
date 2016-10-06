fdb4acd2760fb77094f4d7ad67b246ae7be56224
Mike Bryant <mikesname@gmail.com>
2016-10-06 11:54:55 +0100

    Test fix: ensure session is auto-closed.

--- ehri-cypher/src/test/java/eu/ehri/project/cypher/ProceduresTest.java
+++ ehri-cypher/src/test/java/eu/ehri/project/cypher/ProceduresTest.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 import org.neo4j.driver.v1.Config;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.Values;
 import org.neo4j.harness.junit.Neo4jRule;
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class ProceduresTest {
 
@@ -22,8 +21,8 @@ public class ProceduresTest {
     @Test
     public void testCountryCodeToName() throws Exception {
         try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.build().withEncryptionLevel(Config
-                .EncryptionLevel.NONE).toConfig())) {
-            StatementResult result = driver.session()
+                .EncryptionLevel.NONE).toConfig()); Session session = driver.session()) {
+            StatementResult result = session
                     .run("CALL eu.ehri.project.cypher.countryCodeToName({code})", Values.parameters("code", "us"));
             assertThat(result.single().get("value").asString(), equalTo("United States"));
         }
@@ -32,12 +31,12 @@ public class ProceduresTest {
     @Test
     public void testLanguageCodeToName() throws Exception {
         try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.build().withEncryptionLevel(Config
-                .EncryptionLevel.NONE).toConfig())) {
-            StatementResult result1 = driver.session()
+                .EncryptionLevel.NONE).toConfig()); Session session = driver.session()) {
+            StatementResult result1 = session
                     .run("CALL eu.ehri.project.cypher.languageCodeToName({code})", Values.parameters("code", "en"));
             assertThat(result1.single().get("value").asString(), equalTo("English"));
 
-            StatementResult result2 = driver.session()
+            StatementResult result2 = session
                     .run("CALL eu.ehri.project.cypher.languageCodeToName({code})", Values.parameters("code", "fra"));
             assertThat(result2.single().get("value").asString(), equalTo("French"));
         }
