aeb31d2d56f8548bdc34147b83b80d5895f5b5f0
Mike Bryant <mikesname@gmail.com>
2016-11-29 12:53:01 +0000

    Add a procedure to coerce property values to a list.
    This is useful since (unfortunately) we have some properties that
    can be either single or array values.

--- ehri-cypher/src/main/java/eu/ehri/project/cypher/Procedures.java
+++ ehri-cypher/src/main/java/eu/ehri/project/cypher/Procedures.java
@@ -4,15 +4,26 @@ import eu.ehri.project.utils.LanguageHelpers;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 
 public class Procedures {
 
     public class Output {
-        public String value;
+        public final String value;
 
         Output(String value) {
+            this.value = value;
+        }
+    }
+
+    public class ListOutput {
+        public final List<Object> value;
+
+        ListOutput(List<Object> value) {
             this.value = value;
         }
     }
@@ -25,5 +36,18 @@ public class Procedures {
     @Procedure("eu.ehri.project.cypher.languageCodeToName")
     public Stream<Output> languageCodeToName(@Name("code") String code) {
         return Stream.of(new Output(LanguageHelpers.codeToName(code)));
+    }
+
+    @Procedure("coerceList")
+    public Stream<ListOutput> toList(@Name("data") Object data) {
+        if (data == null) {
+            return Stream.of(new ListOutput(Collections.emptyList()));
+        } else if (data instanceof List) {
+            @SuppressWarnings("unchecked") List<Object> out = (List<Object>) data;
+            return Stream.of(new ListOutput(out));
+        } else if (data instanceof Object[]) {
+            return Stream.of(new ListOutput(Arrays.asList(((Object[]) data))));
+        }
+        return Stream.of(new ListOutput(Collections.singletonList(data)));
     }
 }
