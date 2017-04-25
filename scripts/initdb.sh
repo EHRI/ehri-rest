7ad0d249a762c8b973f5fb0e223c7deaee013953
Mike Bryant <mikesname@gmail.com>
2017-04-25 16:46:24 +0100

    Fix unset variable when no user given.
    I hate bash.

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-set -e
-
 echo "Initializing DB: $NEO4J_HOME/data/databases/graph.db"
 
 $NEO4J_HOME/scripts/cmd initialize
 $NEO4J_HOME/scripts/cmd gen-schema
 
-if [ ! -z "$ADMIN_USER" ]; then
+ADMIN_USER=${ADMIN_USER:-""}
+
+if [ "$ADMIN_USER" != "" ]; then
     echo "Adding administrative user: $ADMIN_USER"
     $NEO4J_HOME/scripts/cmd useradd $ADMIN_USER --group admin
 fi
