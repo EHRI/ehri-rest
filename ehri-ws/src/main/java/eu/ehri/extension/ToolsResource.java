c836a057130714ad9ef013e82026e87cd8c35f03
Mike Bryant <mikesname@gmail.com>
2021-08-30 13:46:12 +0100

    Add tolerant option to batch delete and relinking methods

--- ehri-ws/src/main/java/eu/ehri/extension/ToolsResource.java
+++ ehri-ws/src/main/java/eu/ehri/extension/ToolsResource.java
@@ -503,6 +503,7 @@ public class ToolsResource extends AbstractResource {
      * another and moves changes the link target of anything linked
      * to <code>from</code> to <code>to</code>.
      *
+     * @param tolerant don't error if items have been deleted
      * @param commit  actually commit changes
      * @param mapping a comma-separated TSV file, excluding headers
      * @return CSV data with each row indicating the source, target, and how many items
@@ -512,7 +513,10 @@ public class ToolsResource extends AbstractResource {
     @Consumes({MediaType.APPLICATION_JSON, CSV_MEDIA_TYPE})
     @Produces({MediaType.APPLICATION_JSON, CSV_MEDIA_TYPE})
     @Path("relink-targets")
-    public Table relink(@QueryParam("commit") @DefaultValue("false") boolean commit, Table mapping) throws DeserializationError {
+    public Table relink(
+            @QueryParam("tolerant") @DefaultValue("false") boolean tolerant,
+            @QueryParam("commit") @DefaultValue("false") boolean commit,
+            Table mapping) throws DeserializationError {
         try (final Tx tx = beginTx()) {
             List<List<String>> done = Lists.newArrayList();
             for (List<String> row : mapping.rows()) {
@@ -522,15 +526,21 @@ public class ToolsResource extends AbstractResource {
                 }
                 String fromId = row.get(0);
                 String toId = row.get(1);
-                Linkable from = manager.getEntity(fromId, Linkable.class);
-                Linkable to = manager.getEntity(toId, Linkable.class);
-                int relinked = 0;
-                for (Link link : from.getLinks()) {
-                    link.addLinkTarget(to);
-                    link.removeLinkTarget(from);
-                    relinked++;
+                try {
+                    Linkable from = manager.getEntity(fromId, Linkable.class);
+                    Linkable to = manager.getEntity(toId, Linkable.class);
+                    int relinked = 0;
+                    for (Link link : from.getLinks()) {
+                        link.addLinkTarget(to);
+                        link.removeLinkTarget(from);
+                        relinked++;
+                    }
+                    done.add(Lists.newArrayList(fromId, toId, String.valueOf(relinked)));
+                } catch (ItemNotFound e) {
+                    if (!tolerant) {
+                        throw e;
+                    }
                 }
-                done.add(Lists.newArrayList(fromId, toId, String.valueOf(relinked)));
             }
 
             if (commit) {
