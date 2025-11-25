41a9e3d470b56c2c486f7df4a099b1aec0eb7d6c
Mike Bryant <mikesname@gmail.com>
2025-11-25 16:16:13 +0000

    Bump version and small tweaks to Docker config
    Fully-qualify Dockerfile base ref

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mvn clean package -DskipTests
-sudo docker build -t ehri/ehri-rest --build-arg GIT_COMMIT=$(git rev-parse HEAD) .
+podman build -t ehri/ehri-rest --build-arg GIT_COMMIT=$(git rev-parse HEAD) .
