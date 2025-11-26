db1efef8aad5863a0eab2eaa6415ee854d5ed025
Mike Bryant <mikesname@gmail.com>
2025-11-26 09:10:40 +0000

    Adjust the build docker script to tag based on version

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
 mvn clean package -DskipTests
-podman build -t ehri/ehri-rest --build-arg GIT_COMMIT=$(git rev-parse HEAD) .
+sudo docker build -t ehri/ehri-rest -t ehri/ehri-rest:$VERSION --build-arg GIT_COMMIT=$(git rev-parse HEAD) .
